7b0bb227fab278ac58e7eedac85c500313e05e07
huangzy <huangzhiyang166@gmail.com>
2016-06-01 18:17:33 +0800

    no message

---
+++ common/modules/fileupload/index.js
@@ -0,0 +1,13 @@
+/**
+ * Author: huangzhiyang
+ * Date: 2016/6/1 18:09
+ * Description: ""
+ */
+function Upload(opt){
+
+};
+Upload.prototype = {
+
+};
+
+module.exports = Upload;
--- src/annual_card/page/publish_card_prod_info/index.js
+++ src/annual_card/page/publish_card_prod_info/index.js
@@ -4,3 +4,42 @@
  * Description: ""
  */
 require("./style.scss");
+var AllCitys = require("COMMON/js/config.province.city.js");
+var SSelect = require("COMMON/js/component.city.select.simple.js"); //写了个简单的select控件
+var MainView = Backbone.View.extend({
+	el : $("#cardContainer"),
+	events : {
+		"focus .textInp" : "onTextInpFocus",
+		"blur .textInp" : "onTextInpBlur"
+	},
+	initialize : function(){
+		this.select = new SSelect({
+			data : AllCitys,
+			provId : "#provSelect",
+			cityId : "#citySelect",
+			onProvChange : function(data){
+				console.log(data);
+			},
+			onCityChange : function(data){
+				console.log(data);
+			}
+		})
+	},
+	onTextInpFocus : function(e){
+		var tarInp = $(e.currentTarget);
+		tarInp.parents(".line").removeClass("error");
+	},
+	onTextInpBlur : function(e){
+		var tarInp = $(e.currentTarget);
+		var val = $.trim(tarInp.val());
+		var parent = tarInp.parents(".line");
+		if(!val) return parent.addClass("error");
+		if(tarInp.hasClass("mobileInp") && !PFT.Util.Validate.typePhone(val)){
+			parent.addClass("error");
+		}
+	}
+});
+
+$(function(){
+	new MainView();
+})
