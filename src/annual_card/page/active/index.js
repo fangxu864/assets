156b16aa6ca12c5ffc85a33242acf80cb12a5354
huangzy <huangzhiyang166@gmail.com>
2016-07-01 11:53:53 +0800

    no message

--- src/annual_card/page/active/index.js
+++ src/annual_card/page/active/index.js
@@ -15,12 +15,15 @@ var MainView = Backbone.View.extend({
 	initialize : function(){
 		this.cardInp = $("#cardNum");
 		this.readCardBtn = $("#readCardBtn");
+		this.idCardInp = $("#idCardInp");
+		this.cardInfoBar = $("#cardInfoBar");
 		this.ReadPhysicsCard = new ReadPhysicsCard({id:"readCardObj"});
 	},
 	onReadCardBtnClick : function(e){
 		var cardval = this.ReadPhysicsCard.read();
 		this.cardInp.val(cardval);
-		if(!cardval) alert("读卡失败");
+		if(!cardval) return alert("读卡失败");
+		this.getCardInfo(cardval,"physics")
 	},
 	onTextInpBlur : function(e){
 		var tarInp = $(e.currentTarget);
@@ -48,6 +51,7 @@ var MainView = Backbone.View.extend({
 		}
 	},
 	getCardInfo : function(card_no,type){
+		var that = this;
 		var tarBtn = this.readCardBtn;
 		if(!card_no || !type) return false;
 		PFT.Util.Ajax(Api.Url.active.checkCard,{
@@ -65,10 +69,14 @@ var MainView = Backbone.View.extend({
 				res = res || {};
 				var data= res.data;
 				if(res.code==200){
-					var needID = data.need_ID;
+					var idCardInp = that.idCardInp;
+					var needID = data.need_ID || "";
 					var virtual_no = data.virtual_no;
 					var physics_no = data.physics_no;
+					idCardInp.attr("validate","idCard:"+needID);
+					that.cardInfoBar.show().html("虚拟卡号："+virtual_no+"<i style='margin:0 10px'></i>"+"物理ID："+physics_no);
 				}else{
+					that.cardInfoBar.hide().html();
 					alert(res.msg || PFT.AJAX_ERROR_TEXT)
 				}
 			}
