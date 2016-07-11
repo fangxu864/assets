58f816254d3fb93c7820833b19a5f79553d1e016
huangzy <huangzhiyang166@gmail.com>
2016-07-11 18:05:24 +0800

    no message

--- src/withdraw_bank/bank-dialog/index.js
+++ src/withdraw_bank/bank-dialog/index.js
@@ -11,15 +11,27 @@ var Main = function(){
 	var that = this;
 	this.dialog = new Dialog({
 		width : 680,
-		height : 370,
 		content : dialog_content,
 		drag : true,
+		speed : 100,
 		onReady : function(){
 			that.bankSelect = $("#bankName");
 			that.provSelect = $("#provSelect");
+			that.citySelect = $("#citySelect");
+			that.subBankSelect = $("#subBranchName");
+			that.bankSelect.on("change",function(e){
+				var bank_id = $(this).val();
+				var city_id = that.citySelect.val();
+				that.getBankBranch(bank_id,city_id);
+			})
 			that.provSelect.on("change",function(e){
 				var provId = that.provSelect.val();
 				that.getCityByProvId(provId);
+			})
+			that.citySelect.on("change",function(e){
+				var bank_id = that.bankSelect.val();
+				var city_id = $(this).val();
+				that.getBankBranch(bank_id,city_id);
 			})
 		}
 	})
@@ -27,6 +39,14 @@ var Main = function(){
 Main.prototype = {
 	__bankList : null,
 	__province : null,
+	__ifCache : true,
+	__cityCache : {},
+	__bankCache : {},
+	serializeParams : function(opt){
+		var res = [];
+		for(var i in opt) res.push(i+"="+opt[i]);
+		return res.join("&");
+	},
 	getBankList : function(){
 		var that = this;
 		PFT.Util.Ajax(Api.url("getList"),{
@@ -57,7 +77,7 @@ Main.prototype = {
 					}
 					that.bankSelect.html(bankHtml);
 					that.provSelect.html(provHtml);
-					that.provSelect.children().first().trigger("click")
+					that.getCityByProvId(province[0]["code"]);
 				}else{
 					alert(res.msg || PFT.AJAX_ERROR_TEXT);
 				}
@@ -66,29 +86,103 @@ Main.prototype = {
 	},
 	getCityByProvId : function(id){
 		if(!id) return false;
-		PFT.Util.Ajax(Api.url("cityList"),{
-			type : "post",
-			params : {
-				province_id : id
-			},
-			loading : function(){},
-			complete : function(){},
-			success : function(res){
-				res = res || {};
-				var data = res.data || {};
-				if(res.code==200){
-
-				}else{
-
-				}
+		var that = this;
+		var ifCache = this.__ifCache;
+		var Cache = this.__cityCache[id];
+		var renderOption = function(list){
+			var html = "";
+			for(var i in list){
+				var d = list[i];
+				var code = d["code"];
+				var name = d["name"];
+				html += '<option data-name="'+name+'" value="'+code+'">'+name+'</option>';
 			}
-		})
+			var bank_id = that.bankSelect.val();
+			if(bank_id) that.getBankBranch(bank_id,list[0]["code"]);
+			return html;
+		};
+		if(ifCache && Cache){
+			that.citySelect.html(renderOption(Cache));
+		}else{
+			PFT.Util.Ajax(Api.url("cityList"),{
+				type : "post",
+				params : {
+					province_id : id
+				},
+				loading : function(){},
+				complete : function(){},
+				success : function(res){
+					res = res || {};
+					var data = res.data || {};
+					var list = data.list;
+					if(res.code==200){
+						that.citySelect.html(renderOption(list));
+						if(ifCache) that.__cityCache[id] = list;
+					}else{
+						alert(res.msg || PFT.AJAX_ERROR_TEXT);
+					}
+				}
+			})
+		}
+	},
+	getBankBranch : function(bank_id,city_id,name,page,size){
+		if(!bank_id || !city_id) return false;
+		var that = this;
+		name = name || "";
+		page = page || 1;
+		size = size || 200;
+		var ifCache = this.__ifCache;
+		var params = this.serializeParams({
+			bank_id : bank_id,
+			city_id : city_id,
+			name : name,
+			page : page,
+			size : size
+		});
+		var Cache = this.__bankCache[params];
+		var renderOption = function(list){
+			var html = "";
+			for(var i in list){
+				var d = list[i];
+				var code = d["code"];
+				var name = d["name"];
+				var phone = d["phone"];
+				html += '<option data-name="'+name+'" data-phone="'+phone+'" value="'+code+'">'+name+'</option>';
+			}
+			return html;
+		};
+		if(ifCache && Cache){
+			that.subBankSelect.html(renderOption(Cache));
+		}else{
+			PFT.Util.Ajax(Api.url("subbranchList"),{
+				type : "post",
+				params : {
+					bank_id : bank_id,
+					city_id : city_id,
+					name : name,
+					page : page,
+					size : size
+				},
+				loading : function(){},
+				complete : function(){},
+				success : function(res){
+					res = res || {};
+					var data = res.data || {};
+					var list = data.list;
+					if(res.code==200){
+						that.subBankSelect.html(renderOption(list));
+						if(ifCache) that.__bankCache[params] = list;
+					}else{
+						alert(res.msg || PFT.AJAX_ERROR_TEXT)
+					}
+				}
+			})
+		}
 	},
 	open : function(){
 		var that = this;
 		this.dialog.open({
 			onAfter : function(){
-
 				if(!that.__bankList && !that.__province){
 					that.getBankList();
 				}
