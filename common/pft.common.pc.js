38ece9cf71a63b6f73539598c4009899030111df
huangzy <huangzhiyang166@gmail.com>
2016-07-28 18:40:06 +0800

    no message

--- common/pft.common.pc.js
+++ common/pft.common.pc.js
@@ -3,10 +3,12 @@
  * Date: 2016/7/15 16:07
  * Description: ""
  */
-require("./css/pc-global/index.scss");
+require("./css/pft-common-pc/index.scss");
 var PFT = window["PFT"] || (window["PFT"]={});
 PFT = require("./pft.common.base")(PFT);
 PFT["Util"]["Browser"] = require("./js/util.browser");
 PFT["Util"]["STip"] = require("./js/util.simple.tip");
+var PageBase = require("./pft.common.pc.base");
+$(function(){ PageBase.init(); })
 
 
---
+++ src_mobile/B/index/index.es6
@@ -0,0 +1,6 @@
+import App from "./App.vue";
+import "./index.scss";
+new Vue({
+	el: 'body',
+	components: { App }
+})
