c636f6f646335bf3941148172d21fc208e818172
huangzy <huangzhiyang166@gmail.com>
2016-07-29 11:34:06 +0800

    no message

---
+++ src/wx_config_setmenu/index.js
@@ -0,0 +1,6 @@
+/**
+ * Author: huangzhiyang
+ * Date: 2016/7/29 11:11
+ * Description: ""
+ */
+require("./select-menu-action");
---
+++ src/wx_config_setmenu/select-menu-action/index.js
@@ -0,0 +1,5 @@
+/**
+ * Author: huangzhiyang
+ * Date: 2016/7/29 11:13
+ * Description: ""
+ */
