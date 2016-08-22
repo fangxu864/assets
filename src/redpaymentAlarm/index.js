732cec75db73a2e2d006d733223eff3d100c4a8d
spring123 <15305903254@163.com>
2016-08-22 17:10:06 +0800

    no message

--- src/redpaymentAlarm/index.js
+++ src/redpaymentAlarm/index.js
@@ -9,6 +9,11 @@ var Pubsub =require("COMMON/js/util.pubsub");
 //更改授信预警阀值部分
 var RedpaymentAlarm =function () {
      this.init();
+     var type=$("#type").attr("value");
+    var aid=$("#parent_id ").attr("value");
+    var fid=$("#son_id").attr("value");
+    var money =addFunction.judugement(type,aid,fid);
+    $(".repayAlarmInp").val(money);
 
 };
 RedpaymentAlarm.prototype = Mixin({
@@ -23,34 +28,51 @@ RedpaymentAlarm.prototype = Mixin({
             speed:100,
             events:{
                 "click .repayAlarmSure" : function () {
+                    var type=$("#type").attr("value");
+                    var aid=$("#parent_id ").attr("value");
+                    var fid=$("#son_id").attr("value");
+                    var money = $(".repayAlarmInp").val();
+                    if(money=="") return false;
+                    $.ajax({
+                        type:"post",
+                        dataType:"json",
+                        data:{
+                            aid:aid,
+                            fid:fid,
+                            type:type,
+                            money:money
+
+                        },
+                        url:"/r/member_ThresHold/setThreshold",
+                        success:function (data) {
+                            if(data.flag==-1){
+                                alert(data.info);
+                            }
+
+                        },
+                        error:function (xhr,msg) {
+
+                        }
+                    });
                   that.dialog.close();
 
                 },
                 "click .repayAlarmCancel":function(){
                   that.dialog.close() ;
+
                 }
 
             },
             onReady:function () {
 
+
             }
         })
            this.dialog.open();
 
+    }
 
 
-    },
-
-    // open:function (opt) {
-    //     var that =this;
-    //     this.dialog.open({
-    //         onAfter:function () {
-    //             var dialog_submitBtn =$("#repaymentAlarmBtn");
-    //             dialog_submitBtn.attr("data-typenum",that.type)
-    //         }
-    //
-    //     })
-    // }
 },Pubsub );
 //授信预警按钮部分
 var addFunction= {
@@ -87,7 +109,10 @@ var addFunction= {
     },
     changeColor:function(){
         var that =this;
+        var state = 0;
         $("#AlarmChangeColorBlack").click(function(){
+            state =2;
+            that.returnDate(state);
             var color = that.RGBtoHEX($('#AlarmChangeColorBlack').css("backgroundColor"));
             if(color =='#cccccc'){
                 $(this).css("background-color","red");
@@ -100,6 +125,8 @@ var addFunction= {
             }
         })
         $("#AlarmChangeColorRed").click(function(){
+            state=1;
+            that.returnDate(state);
             var color =that.RGBtoHEX($('#AlarmChangeColorRed').css("backgroundColor"));
             if(color =='#cccccc'){
                 $(this).css("background-color","black");
@@ -111,31 +138,48 @@ var addFunction= {
             }
         })
     },
+
+
     judugement:function(type,aid,fid){
-           $.ajax({
+        $.ajax({
                 type:"post",
                 dataType:"json",
                 data:{
                     aid:aid,
                     fid:fid,
-                   type:type
+                   type:type,
+
                 },
                 url:"/r/member_ThresHold/getBalance",
                 success:function (data) {
-                    if(data.type==2){
-                        $("#AlarmChangeColorRed").css("background","black")
-                       $("#AlarmChangeColorBlack").css("background","#cccccc")
+                    if(data.flag==-1){
+                      alert(data.info);
+
                     }
-                    else if(date.type==1){
+                    else if(date.date==1){
                         $("#AlarmChangeColorBlack").css("background","red")
                         $("#AlarmChangeColorRed").css("background","#cccccc")
                     }
+                    else if(data.date==0){
+                        $("#AlarmChangeColorRed").css("background","black");
+                        $("#AlarmChangeColorBlack").css("background","#cccccc")
+                    }
+                    var money = date.balance;
+                    money=1000;
+                    $("<input id='addAlarmPart' type='text'>").val(money);
+                    alert($("#addAlarmPart"));
                 },
                 error:function (xhr,msg) {
-                    alert(msg);
+                    // var money = date.balance;
+                   var money=1000;
+                   $("<input id='addAlarmPart' type='text'>").val(money);
+                    alert($("#addAlarmPart").val());
                 }
-            });
 
+
+            })
+
+        return money;
 
         },
     getDate:function(){
@@ -144,22 +188,44 @@ var addFunction= {
         var aid=$("#parent_id ").attr("value");
         var fid=$("#son_id").attr("value");
         that.judugement(type,aid,fid);
-        console.log(type+aid+fid);
+
+
+
+    },
+    returnDate:function(state){
+        var that = this;
+        var type=$("#type").attr("value");
+        var aid=$("#parent_id ").attr("value");
+        var fid=$("#son_id").attr("value");
+        $.ajax({
+            type:"post",
+            dataType:"json",
+            data: {
+                aid: aid,
+                fid: fid,
+                type: type,
+                state: state
+            },
+            success:function(date){
+                alert(date)
+            },
+            error:function(xrh,msg){
+                alert(msg)
+            }
+
+        })
     }
-
-
 
 }
 
 
 $(function () {
-    addFunction.getDate();
+   addFunction.getDate();
    addFunction.changeColor();
   $("#repaymentAlarm").click(function(){
-       new RedpaymentAlarm()
+       new RedpaymentAlarm();
+
 })
-
-
 
 
 })
