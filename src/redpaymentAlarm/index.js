3c91f1ea018944f170552fcd8c8e782f4548f670
spring123 <15305903254@163.com>
2016-08-22 09:41:38 +0800

    no message

--- src/redpaymentAlarm/index.js
+++ src/redpaymentAlarm/index.js
@@ -41,41 +41,89 @@ RedpaymentAlarm.prototype = Mixin({
 
     },
 
-    open:function (opt) {
-        var that =this;
-        this.dialog.open({
-            onAfter:function () {
-                var dialog_submitBtn =$("#repaymentAlarmBtn");
-                dialog_submitBtn.attr("data-typenum",that.type)
-            }
-
-        })
-    }
+    // open:function (opt) {
+    //     var that =this;
+    //     this.dialog.open({
+    //         onAfter:function () {
+    //             var dialog_submitBtn =$("#repaymentAlarmBtn");
+    //             dialog_submitBtn.attr("data-typenum",that.type)
+    //         }
+    //
+    //     })
+    // }
 },Pubsub );
 
 var addFunction= {
-       changecolor:function () {
+    RGBtoHEX:function (str) {
+        var that =  this;
+        if(str.substring(0, 3) == 'rgb'){
+            var arr = str.split(",");
+            var r = arr[0].replace('rgb(','').trim();
+            var g = arr[1].trim();
+            var b = arr[2].replace(')','').trim();
+            var hex =[
+             that.toHex(r),
+             that.toHex(g),
+             that.toHex(b)
+            ];
+            return "#"+hex.join('');
 
-            $("#AlarmChangeColorRed").click(function(){
-               alert($("#AlarmChangeColorRed").css("background"))
-                if($("#AlarmChangeColorRed").css("background")=="red"||$("#AlarmChangeColorRed").css("background")=="#ccc"){
-                    $("#AlarmChangeColorRed").css("background","#ccc")
-                    $("#AlarmChangeColorBlack").css("background","#333333")
-                }
-            })
-           $("#AlarmChangeColorBlack").click(function () {
-               if($(this).css("background")=="#333333"){
-                   $("#AlarmChangeColorRed").css("background","red");
-                   $("#AlarmChangeColorBlack").css("background","#ccc");
-               }
-           })
+        }
+        else{
+            return str;
+        }
+        
+    },
+    toHex:function (N) {
+        if(N==null) return "00";
+        N = parseInt(N);
+        if(N==0||isNaN(N)) return"00";
+        N=Math.max(0,N);
+        N = Math.min(N,255);
+        N = Math.round(N);
+        return "0123456789abcdef".charAt((N-N%16)/16)+"0123456789abcdef".charAt(N%16);
+        //　return "0123456789ABCDEF".charAt((N-N%16)/16) + "0123456789ABCDEF".charAt(N%16);  //大写
+        
+    },
+    changeColor:function(){
+        var that =this;
+        $("#AlarmChangeColorBlack").click(function(){
+            var color = that.RGBtoHEX($('#AlarmChangeColorBlack').css("backgroundColor"));
+            if(color =='#cccccc'){
+                $(this).css("background-color","red");
+                $("#AlarmChangeColorRed").css("backgroundColor","#cccccc");
 
-       }
+            }
+            else{
+                $(this).css("backgroundColor","#cccccc");
+                $("#AlarmChangeColorRed").css("backgroundColor","black")
+            }
+        })
+        $("#AlarmChangeColorRed").click(function(){
+            var color =that.RGBtoHEX($('#AlarmChangeColorRed').css("backgroundColor"));
+            if(color =='#cccccc'){
+                $(this).css("background-color","black");
+                $("#AlarmChangeColorBlack").css("backgroundColor","#cccccc")
+            }
+            else{
+                $(this).css("backgroundColor","#cccccc");
+                $("#AlarmChangeColorBlack").css("backgroundColor","red")
+            }
+        })
+    },
+    judugement:function(){
+        $.ajax()
+    }
+
+
+
 }
 
 
 $(function () {
-addFunction.changecolor();
+
+addFunction.changeColor();
+    $("#AlarmChangeColorRed").css("background","black")
 $("#repaymentAlarm").click(function(){
 
        new RedpaymentAlarm()
