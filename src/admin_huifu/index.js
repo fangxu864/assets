df303d9f6feb819c599b1b9eabd7cb44f98c7815
spring123 <15305903254@163.com>
2016-08-23 19:04:48 +0800

    no message

--- src/admin_huifu/index.js
+++ src/admin_huifu/index.js
@@ -1,3 +1,46 @@
 /**
  * Created by Administrator on 2016/8/23.
  */
+var recoverBtn;
+recoverBtn={
+    sendRrcoverBtn:function(){
+              $(".set_state").each(function () {
+                $(this).click(function () {
+                    if($(this).attr("data-state")==7){
+                        var data=$(this).attr("data-pid");
+                        $.ajax({
+                            type:"post",
+                            dataType:"json",
+                            data:{
+                                data:data
+                            },
+                            url:"",
+                            success:function(data){
+                                alert(data.info);
+                            },
+                            error:function(xrh,msg){
+                                alert(msg);
+                            }
+                        })
+                        return false;
+
+                    }
+                    else{
+                        return false;
+                    }
+                   
+                })
+            })
+
+
+
+    }
+
+}
+
+
+
+
+$(function () {
+    recoverBtn .sendRrcoverBtn();
+})
