414ae240d2d46e58c0e92888804656ccbbc850ce
spring123 <15305903254@163.com>
2016-08-26 16:40:56 +0800

    no message

---
+++ src/schedule_report/webpack.config.js
@@ -0,0 +1,17 @@
+/**
+ * Author: huangzhiyang
+ * Date: 2016/7/11 11:17
+ * Description: ""
+ */
+var path = require("path");
+var env = require("../../task-webpack/getNodeENV.js");
+var plugins = require("../../task-webpack/getPlugins")(env);
+var output = require("../../task-webpack/getOutput")(env);
+var config = require("../../task-webpack/config")({
+	entry : {
+		"schedule" : "./src/schedule_report/index.js"
+	},
+	output : output,
+	plugins : plugins
+});
+module.exports = config;
