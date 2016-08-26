004355ab967b3423de7c536590ff0cf2937d13f7
spring123 <15305903254@163.com>
2016-08-26 16:22:00 +0800

    no message

--- src/redpaymentAlarm/index.js
+++ src/redpaymentAlarm/index.js
@@ -53,7 +53,7 @@ RedpaymentAlarm.prototype = Mixin({
                             money:money
 
                         },
-                        url:"/r/Member_ThresHold/setThreshold",
+                        url:"/r/Member_ThresHold/setThreshold/",
                         success:function (data) {
                             if(data.flag==-1){
                                 alert(data.info);
@@ -179,7 +179,7 @@ var addFunction= {
                     type:type
 
                 },
-                url:"/r/Member_ThresHold/getBalance",
+                url:"/r/Member_ThresHold/getBalance/",
                 success:function (data) {
                     Pubsub.trigger("ajax.success",data.balance);
                     if(data.flag== -1){
@@ -233,7 +233,7 @@ var addFunction= {
                 type: type,
                 state:state
             },
-        url:"/r/Member_ThresHold/setState",
+        url:"/r/Member_ThresHold/setState/",
             success:function(data){
                 Pubsub.trigger("ajax.success",data.flag);
                if(data.flag==1){
