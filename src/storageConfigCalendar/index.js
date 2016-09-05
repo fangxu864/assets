d59f453572e17e48a5d91b828c7f4d4b50aab602
spring123 <15305903254@163.com>
2016-09-05 16:19:41 +0800

    no message

---
+++ src/storageConfigCalendar/index.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2016/9/5.
+ */
+var Calendar = require("COMMON/modules/calendar");
+$(function () {
+    AddCalendar.init();
+});
+var AddCalendar ={
+    init:function(){
+            var that= this;
+            that.Calendar = new Calendar();
+            that.CreatCalendar("#storage_open")
+    },
+    CreatCalendar:function(id){
+            var that = this;
+            $(id).on("focus",function (e) {
+                var picker = $(e.target);
+                var date = picker.val();
+                that.Calendar.show(date,{
+                    picker:$(e.target),
+                    top:0,
+                    left:0,
+                    min:"2016-06-20",
+                    max:"2016-09-30",
+                    onBefore:function(){
+
+                    },
+                    onAfter:function(){
+
+                    }
+                });
+            })
+
+    }
+
+}
