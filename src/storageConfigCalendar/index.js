864efa29a97073f46fe14d7d052ab8f091e59f44
spring123 <15305903254@163.com>
2016-09-05 16:48:25 +0800

    no message

--- src/storageConfigCalendar/index.js
+++ src/storageConfigCalendar/index.js
@@ -9,17 +9,19 @@ var AddCalendar ={
     init:function(){
             var that= this;
             that.Calendar = new Calendar();
-            that.CreatCalendar("#storage_open")
+            that.CreatCalendar("#storage_open");
+
     },
     CreatCalendar:function(id){
             var that = this;
             $(id).on("focus",function (e) {
+                alert("fdsafjlds")
                 var picker = $(e.target);
                 var date = picker.val();
                 that.Calendar.show(date,{
                     picker:$(e.target),
-                    top:0,
-                    left:0,
+                    top:100,
+                    left:200,
                     min:"2016-06-20",
                     max:"2016-09-30",
                     onBefore:function(){
