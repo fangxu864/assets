adcefb733fc539c7057c98098c186e72656c3e99
fangxu <864109504@qq.com>
2016-09-23 15:01:14 +0800

    no message

--- src/annual_card/webpack.config.js
+++ src/annual_card/webpack.config.js
@@ -11,13 +11,13 @@ var config = require("../../task-webpack/config")({
 		//"annual_card_publish_prod_info" : "./src/annual_card/page/publish_prod_info/index.js",
 		//"annual_card_publish_package_info" : "./src/annual_card/page/publish_package_info/index.js",
 		//"annual_card_entry_card" : "./src/annual_card/page/entry_card/index.js",
-		"annual_card_makeorder" : "./src/annual_card/page/makeorder/index.js",
+		// "annual_card_makeorder" : "./src/annual_card/page/makeorder/index.js",
 		//"annual_card_buy_dialog" : "./src/annual_card/page/yorderlist/buy.dialog.js",
 		//"annual_card_storage" : "./src/annual_card/page/storage/index.js",
 		//"annual_card_ordersuccess" : "./src/annual_card/page/ordersuccess/index.js",
 		//"annual_card_active" : "./src/annual_card/page/active/index.js",
 		//"annual_memdetail" : "./src/annual_card/page/memdetail/index.js",
-		//"annual_card_mclist" : "./src/annual_card/page/mclist/index.js"
+		"annual_card_mclist" : "./src/annual_card/page/mclist/index.js"
 	},
 	output : output,
 	plugins : plugins,
