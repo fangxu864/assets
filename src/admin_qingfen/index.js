48aab8726a5513bb2e346489a6dbcda6aecfff7d
fangxu <864109504@qq.com>
2016-09-27 17:22:55 +0800

    no message

--- src/admin_qingfen/index.js
+++ src/admin_qingfen/index.js
@@ -3,3 +3,24 @@
  */
 
 require("./index.scss");
+//引入各种tpl
+var tableCon_tpl=require("./tpl/tableCon.xtpl");
+
+
+var admin_qingfen={
+    init:function () {
+        var _this=this;
+        //获取内容盒子
+        this.tableCon_box=$(".tableCon_box");
+        //往盒子中添加内容
+        this.tableCon_box.html(tableCon_tpl);
+    },
+    bind:{
+        
+    },
+    
+}
+
+$(function () {
+    admin_qingfen.init();
+})
