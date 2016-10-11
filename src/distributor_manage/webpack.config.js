01b1bec05e37eb167943fb66af52811ba01b0350
fangxu <864109504@qq.com>
2016-10-11 17:09:11 +0800

    分销商管理

--- src/distributor_manage/webpack.config.js
+++ src/distributor_manage/webpack.config.js
@@ -1,14 +1,15 @@
 /**
  * Created by Administrator on 15-12-4.
  */
+var env = require("../../task-webpack/getNodeENV");
 var path = require("path");
-module.exports = {
+var plugins = require("../../task-webpack/getPlugins")(env);
+var output = require("../../task-webpack/getOutput")(env);
+var config = require("../../task-webpack/config")({
 	entry : {
-		"distributor.manager" : "./js/distributor.manager.js"
+		"distributor_manage" : "./src/distributor_manage/js/index.js"
 	},
-	output : {
-		path: path.join(__dirname, "./js/build/v0.1"),
-		filename: "[name].all.js"
-	},
-	watch : true
-}
+	output : output,
+	plugins : plugins
+});
+module.exports = config;
