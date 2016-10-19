931e187fa62f0da577be4798b3bab44ca9e05857
huangzy <huangzhiyang166@gmail.com>
2016-10-19 09:32:00 +0800

    no message

--- src_mobile/C/index/search/index.js
+++ src_mobile/C/index/search/index.js
@@ -4,15 +4,34 @@
  * Description: ""
  */
 require("./index.scss");
+var ContainerTpl = require("./index.xtpl");
+var Service = require("SERVICE_M/getproduct-hot-c");
+var Loading = require("COMMON/js/util.loading.pc")("努力加载中...",{
+	tag : "li",
+	className : "sta loading"
+});
 var Search = PFT.Util.Class({
 	status : "close",
+	__timeout : null,
 	init : function(){
-		var container = this.container = $('<div id="searchListPage" class="searchListPage"></div>');
+		var that = this;
+		var container = this.container = $(ContainerTpl);
 		this.searchBtn = $("#searchBtn");
 		this.userBtn = $("#userBtn");
 		this.searchInp = $("#searchInp");
 		this.$body = $("body");
 		this.$body.append(container);
+		this.historyContainer = container.children(".historyContainer");
+		this.resultContainer = container.children(".searchResultContainer");
+
+		this.searchInp.on("input",function(e){
+			clearTimeout(that.__timeout);
+			that.__timeout = setTimeout(function(){
+				that.onSearchInpChange(e);
+			},300)
+		})
+
+
 		this.initRouter();
 	},
 	initRouter : function(){
@@ -38,9 +57,59 @@ var Search = PFT.Util.Class({
 	setSearchHistory : function(keyword){
 
 	},
+	queryByKeyword : function(keyword){
+		var that = this;
+		if(keyword==""){
+			this.showHistory();
+			this.hideSearchResult();
+			this.getSearchHistory();
+		}else{
+			this.hideHistory();
+			this.showSearchResult();
+			var listUl = this.resultContainer;
+
+
+			Service({
+				keyword : keyword,
+				loading : function(){
+					listUl.html(Loading);
+				},
+				complete : function(){
+					listUl.html("");
+				},
+				success : function(data){
+
+				},
+				empty : function(){
+					listUl.html('<li class="sta empty">查无匹配产品...</li>');
+				},
+				fail : function(msg){
+					listUl.html('<li class="sta fail">'+msg+'</li>');
+				}
+			},this)
+		}
+	},
+	showHistory : function(){
+		this.historyContainer.show();
+	},
+	hideHistory : function(){
+		this.historyContainer.hide();
+	},
+	showSearchResult : function(){
+		this.resultContainer.show();
+	},
+	hideSearchResult : function(){
+		this.resultContainer.hide();
+	},
+	onSearchInpChange : function(e){
+		var tarInp = $(e.target);
+		var keyword = $.trim(tarInp.val());
+		this.queryByKeyword(keyword);
+	},
 	show : function(){
 		var that = this;
 		var container = this.container;
+		var searchInp = this.searchInp;
 		if(this.status=="show") return false;
 		container[0].style.display = "block";
 		this.searchBtn.removeClass("hide");
@@ -48,6 +117,10 @@ var Search = PFT.Util.Class({
 		container.animate({"transform" : "translate3d(0px,0px,0px)"},300,"linear",function(){
 			that.status = "show";
 		});
+
+		var keyword = $.trim(this.searchInp.val());
+		this.queryByKeyword(keyword);
+
 	},
 	close : function(){
 		var that = this;
