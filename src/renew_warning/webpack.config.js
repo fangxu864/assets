aa501581facc11b8f7be70c7eb893c43ca559ee3
fangxu <864109504@qq.com>
2016-10-27 11:24:55 +0800

    no message

---
+++ src/renew_warning/webpack.config.js
@@ -0,0 +1,18 @@
+/**
+ * Author: huangzhiyang
+ * Date: 2016/6/17 16:42
+ * Description: ""
+ */
+var env = require("../../task-webpack/getNodeENV.js");
+var output = require("../../task-webpack/getOutput")(env,"[name]/all");
+var plugins = require("../../task-webpack/getPlugins")(env,"[name]/all");
+var config = require("../../task-webpack/config")({
+	entry : {
+		//续费通知
+		"renew_warning_xufeichongzhi" : "./src/renew_warning/xufeichongzhi/index.js"
+	},
+	output : output,
+	plugins : plugins,
+	watch : true
+});
+module.exports = config;
---
+++ src/renew_warning/xufeichongzhi/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fangxu  on 2016/10/26.
+ */
+
+
+//引入css
+require("./index.scss");
+//引入tpl
+require("./index.xtpl");
+
+alert("ok");
