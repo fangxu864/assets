34fd76c9e21eb22ab77313c1af934a31fc6c21a4
fangxu <864109504@qq.com>
2016-11-02 09:56:38 +0800

    续费通知

--- src/renew_warning/xufeichongzhi/dialog/dialog.js
+++ src/renew_warning/xufeichongzhi/dialog/dialog.js
@@ -37,13 +37,50 @@ Dialog.prototype={
         this.Dialog_simple.open()
     },
     show_dialog_con:function (data) {
+        var _this=this;
         var list=data;
         var _this=this;
         var html=_this.template({data:list});
         this.Dialog_box.html(html);
         this.open()
+        _this.ajaxGetVcode(list)
     },
-    template:PFT.Util.ParseTemplate(dialog_con_tpl)
+    template:PFT.Util.ParseTemplate(dialog_con_tpl),
+    ajaxGetVcode:function (data) {
+        $.ajax({
+            url: data.url,    //请求的url地址
+            dataType: "json",   //返回格式为json
+            async: true, //请求是否异步，默认为异步，这也是ajax重要特性
+            data: { "id": data.typeid },    //参数值
+            type: "post",   //请求方式
+            timeout:10000,   //设置超时 10000毫秒
+            beforeSend: function() {
+                //请求前的处理
+            },
+            success: function(res) {
+                //请求成功时处理
+                console.log(res);
+                $("#payCode_box").html("");
+                new QRCode("payCode_box",{
+                    text:res.data.qrUrl,
+                    width:200,
+                    height:200,
+                    colorDark:"#000000",
+                    colorLight:"#ffffff",
+                    correctLevel:QRCode.CorrectLevel.H
+                })
+            },
+            complete: function(res,status) {
+                //请求完成的处理
+                if(status=="timeout"){
+                    alert("请求超时")
+                }
+            },
+            error: function() {
+                //请求出错处理
+            }
+        });
+    }
 };
 
 module.exports=Dialog;
