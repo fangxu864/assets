6fa710e7c7f22bdb2b59188994356b000bfb3338
houxingyi <1026043046@qq.com>
2016-11-04 16:57:54 +0800

    新版销售报表优化（分销商名称与景区门票名称分开，验证报表加平均价格）

--- src/sale_reportform/report_checked/index.js
+++ src/sale_reportform/report_checked/index.js
@@ -480,54 +480,138 @@ var Book_form={
         };
         var kindsTitle=titleName[_this.filterParamsBox.count_way];
 
-        //计算平均价格
-        var sale_money,ticket_num,average;
-        for (var i = 0; i<list.length; i++) {
-            sale_money = list[i].sale_money;
-            ticket_num = list[i].ticket_num;
-            average = sale_money/ticket_num;
-            average = Math.ceil(average*100);
-            average = average/100;
-            list[i].average = average;
-        }
         if(list[0]["profit_money"]!==undefined){    //非景区账号
-            theadHtml='<th class="th1">'+kindsTitle+'</th> <th class="th2">订单数</th> <th class="th3">检票数</th> <th class="th4">平均价格</th><th class="th5">金额(元)</th> <th class="th6">利润(元)</th>';
-            $(".tablecon_box .con_tb thead tr").html(theadHtml);
-            listHtml+='<tr> <td class="th1 heji">合计:</td>'+
-                '<td class="th2">'+sum.orderNum+'</td>'+
-                '<td class="th3">'+sum.ticketNum+'</td>'+
-                '<td class="th4"></td>'+
-                '<td class="th5">'+sum.saleMoney+'</td>'+
-                '<td class="th6">'+sum.profitMoney+'</td>'+
-                '</tr>';
-            for(var i=0;i<list.length;i++){
-                listHtml+='<tr> <td class="th1">'+list[i].title+'</td>'+
-                    '<td class="th2">'+list[i].order_num+'</td>'+
-                    '<td class="th3">'+list[i].ticket_num+'</td>'+
-                    '<td class="th4">'+list[i].average+'</td>'+
-                    '<td class="th5">'+list[i].sale_money+'</td>'+
-                    '<td class="th6">'+list[i].profit_money+'</td>'+
-                    '</tr>'
+            if(kindsTitle == "分销商/票类"){     //分销商票类页面单独渲染
+                theadHtml='<th class="th1">分销商名称</th> <th class="th2">景区门票名称</th><th class="th3">订单数</th> <th class="th4">检票数</th> <th class="th5">平均价格</th> <th class="th6">金额(元)</th> <th class="th7">利润(元)</th> ';
+                $(".tablecon_box .con_tb thead tr").html(theadHtml);
+                listHtml+='<tr> <td class="th2 heji" colspan="2">合计:</td>'+
+                    '<td class="th3">'+sum.orderNum+'</td>'+
+                    '<td class="th4">'+sum.ticketNum+'</td>'+
+                    '<td class="th5"></td>'+
+                    '<td class="th6">'+sum.saleMoney+'</td>'+
+                    '<td class="th7">'+sum.profitMoney+'</td>'+
+                    '</tr>';
+                for(var i=0;i<list.length;i++){
+
+                    listHtml+='<tr> <td class="th1">'+list[i].reseller_title+'</td>'+
+                        '<td class="th2"></td>'+
+                        '<td class="th3">'+list[i].order_num+'</td>'+
+                        '<td class="th4">'+list[i].ticket_num+'</td>'+
+                        '<td class="th5"></td>'+
+                        '<td class="th6">'+list[i].sale_money+'</td>'+
+                        '<td class="th7">'+list[i].profit_money+'</td>'+
+                        '</tr>';
+                    for(var j=0;j<list[i].tickets.length;j++){
+                        listHtml+='<tr> <td class="th1"></td>'+
+                            '<td class="th2">'+list[i].tickets[j].title+'</td>'+
+                            '<td class="th3">'+list[i].tickets[j].order_num+'</td>'+
+                            '<td class="th4">'+list[i].tickets[j].ticket_num+'</td>'+
+                            '<td class="th5">'+list[i].tickets[j].avg_sale_money+'</td>'+
+                            '<td class="th6">'+list[i].tickets[j].sale_money+'</td>'+
+                            '<td class="th7">'+list[i].tickets[j].profit_money+'</td>'+
+                            '</tr>';
+
+                    }
+
+                }
+                $(".tablecon_box .con_tb tbody").html(listHtml);
+                $(".tablecon_box .con_tb *").addClass("resellerAndTicket");     //全部加class以区分
+                $(".tablecon_box .con_tb tbody .th5").css("color","#000");
+                $(".tablecon_box .con_tb tbody .th6").css("color","#3DBA3F");
+                $(".tablecon_box .con_tb tbody .th7").css("color","#F07845");
+            }else{
+                theadHtml='<th class="th1">'+kindsTitle+'</th> <th class="th2">订单数</th> <th class="th3">检票数</th> <th class="th4">平均价格</th><th class="th5">金额(元)</th> <th class="th6">利润(元)</th>';
+                $(".tablecon_box .con_tb thead tr").html(theadHtml);
+                listHtml+='<tr> <td class="th1 heji">合计:</td>'+
+                    '<td class="th2">'+sum.orderNum+'</td>'+
+                    '<td class="th3">'+sum.ticketNum+'</td>'+
+                    '<td class="th4"></td>'+
+                    '<td class="th5">'+sum.saleMoney+'</td>'+
+                    '<td class="th6">'+sum.profitMoney+'</td>'+
+                    '</tr>';
+                for(var i=0;i<list.length;i++){
+                    listHtml+='<tr> <td class="th1">'+list[i].title+'</td>'+
+                        '<td class="th2">'+list[i].order_num+'</td>'+
+                        '<td class="th3">'+list[i].ticket_num+'</td>'+
+                        '<td class="th4">'+list[i].avg_sale_money+'</td>'+
+                        '<td class="th5">'+list[i].sale_money+'</td>'+
+                        '<td class="th6">'+list[i].profit_money+'</td>'+
+                        '</tr>'
+
+                }
+                $(".tablecon_box .con_tb tbody").html(listHtml);
+                $(".tablecon_box .con_tb tbody tr:odd").addClass("gray");
+                $(".tablecon_box .con_tb tbody .th4").css("color","#000");
+                $(".tablecon_box .con_tb tbody .th5").css("color","#3DBA3F");
+                $(".tablecon_box .con_tb tbody .th6").css("color","#F07845");
             }
-            $(".tablecon_box .con_tb tbody").html(listHtml);
+
+
         }else{
-            theadHtml='<th class="th1">'+kindsTitle+'</th> <th class="th2">订单数</th> <th class="th3">检票数</th> <th class="th4">金额(元)</th> ';
-            $(".tablecon_box .con_tb thead tr").html(theadHtml);
-            listHtml+='<tr> <td class="th1 heji">合计:</td>'+
-                '<td class="th2">'+sum.orderNum+'</td>'+
-                '<td class="th3">'+sum.ticketNum+'</td>'+
-                '<td class="th4">'+sum.saleMoney+'</td>'+
-                '</tr>';
-            for(var i=0;i<list.length;i++){
-                listHtml+='<tr> <td class="th1">'+list[i].title+'</td>'+
-                    '<td class="th2">'+list[i].order_num+'</td>'+
-                    '<td class="th3">'+list[i].ticket_num+'</td>'+
-                    '<td class="th4">'+list[i].sale_money+'</td>'+
-                    '</tr>'
+            if(kindsTitle == "分销商/票类"){     //分销商票类页面单独渲染
+                theadHtml='<th class="th1">分销商名称</th> <th class="th2">景区门票名称</th><th class="th3">订单数</th> <th class="th4">检票数</th> <th class="th5">平均价格</th> <th class="th6">金额(元)</th> <th class="th7">利润(元)</th> ';
+                $(".tablecon_box .con_tb thead tr").html(theadHtml);
+                listHtml+='<tr> <td class="th2 heji" colspan="2">合计:</td>'+
+                    '<td class="th3">'+sum.orderNum+'</td>'+
+                    '<td class="th4">'+sum.ticketNum+'</td>'+
+                    '<td class="th5"></td>'+
+                    '<td class="th6">'+sum.saleMoney+'</td>'+
+                    '<td class="th7">'+sum.profitMoney+'</td>'+
+                    '</tr>';
+                for(var i=0;i<list.length;i++){
+
+                    listHtml+='<tr> <td class="th1">'+list[i].reseller_title+'</td>'+
+                        '<td class="th2"></td>'+
+                        '<td class="th3">'+list[i].order_num+'</td>'+
+                        '<td class="th4">'+list[i].ticket_num+'</td>'+
+                        '<td class="th5"></td>'+
+                        '<td class="th6">'+list[i].sale_money+'</td>'+
+                        '<td class="th7">'+list[i].profit_money+'</td>'+
+                        '</tr>';
+                    for(var j=0;j<list[i].tickets.length;j++){
+                        listHtml+='<tr> <td class="th1"></td>'+
+                            '<td class="th2">'+list[i].tickets[j].title+'</td>'+
+                            '<td class="th3">'+list[i].tickets[j].order_num+'</td>'+
+                            '<td class="th4">'+list[i].tickets[j].ticket_num+'</td>'+
+                            '<td class="th5">'+list[i].tickets[j].avg_sale_money+'</td>'+
+                            '<td class="th6">'+list[i].tickets[j].sale_money+'</td>'+
+                            '<td class="th7">'+list[i].tickets[j].profit_money+'</td>'+
+                            '</tr>';
+
+                    }
+
+                }
+                $(".tablecon_box .con_tb tbody").html(listHtml);
+                $(".tablecon_box .con_tb *").addClass("resellerAndTicket");     //全部加class以区分
+                $(".tablecon_box .con_tb tbody .th5").css("color","#000");
+                $(".tablecon_box .con_tb tbody .th6").css("color","#3DBA3F");
+                $(".tablecon_box .con_tb tbody .th7").css("color","#F07845");
+            }else{
+                theadHtml='<th class="th1">'+kindsTitle+'</th> <th class="th2">订单数</th> <th class="th3">检票数</th> <th class="th4">金额(元)</th> ';
+                $(".tablecon_box .con_tb thead tr").html(theadHtml);
+                listHtml+='<tr> <td class="th1 heji">合计:</td>'+
+                    '<td class="th2">'+sum.orderNum+'</td>'+
+                    '<td class="th3">'+sum.ticketNum+'</td>'+
+                    '<td class="th4">'+sum.saleMoney+'</td>'+
+                    '</tr>';
+                for(var i=0;i<list.length;i++){
+                    listHtml+='<tr> <td class="th1">'+list[i].title+'</td>'+
+                        '<td class="th2">'+list[i].order_num+'</td>'+
+                        '<td class="th3">'+list[i].ticket_num+'</td>'+
+                        '<td class="th4">'+list[i].sale_money+'</td>'+
+                        '</tr>'
+                }
+                $(".tablecon_box .con_tb tbody").html(listHtml);
+                $(".tablecon_box .con_tb tbody tr:odd").addClass("gray");
+                $(".tablecon_box .con_tb tbody .th4").css("color","#000");
+                $(".tablecon_box .con_tb tbody .th5").css("color","#3DBA3F");
+                $(".tablecon_box .con_tb tbody .th6").css("color","#F07845");
             }
-            $(".tablecon_box .con_tb tbody").html(listHtml);
         }
-        $(".tablecon_box .con_tb tbody tr:odd").addClass("gray");
+
+
+
+
         _this.tablecon_box.fadeIn(200);
     },
     //处理分页器
