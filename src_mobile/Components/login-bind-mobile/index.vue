a60e4e2bd2c6008c8792b1f7adf623c2f64d412a
fangxu <864109504@qq.com>
2016-11-17 18:21:07 +0800

    wx

--- src_mobile/Components/login-bind-mobile/index.vue
+++ src_mobile/Components/login-bind-mobile/index.vue
@@ -3,14 +3,14 @@
         <div class="mainCon">
             <div class="form-group top mobile">
                 <div class="inputBox">
-                    <input v-model="mobile" class="mobile" type="text" name="" autocomplete="off" id="mobileInp" placeholder="手机号"/>
+                    <input v-model="mobile" @blur="onMobileInpBlur" class="mobile" type="text" name="" autocomplete="off" id="mobileInp" placeholder="手机号"/>
                 </div>
                 <span class="icon mobile"><i class="uicon uicon-shouji"></i></span>
             </div>
             <div class="form-group bottom checkMa">
                 <div class="inputBox"><input v-model="code" class="checkMa" type="number" autocomplete="off" name="" id="checkMaInp" placeholder="验证码"/></div>
                 <span class="icon pwd"><i class="uicon uicon-key-sin"></i></span>
-                <a id="getCheckMaBtn" @click="onGetCheckMaBtnClick" class="getCheckMaBtn" :class="vcodeClass" href="javascript:void(0)" v-text="vcodeBtnText"></a>
+                <a id="getCheckMaBtn" @click="onGetCheckMaBtnClick" class="getCheckMaBtn disable" :class="vcodeClass" href="javascript:void(0)" v-text="vcodeBtnText"></a>
             </div>
             <a id="submitBtn_mobile" @click="onSubmit" class="submitBtn submitBtn_mobile mobile" href="javascript:void(0)">绑定</a>
             <p id="errorTip_mobile" class="errorTip mobile"></p>
@@ -21,10 +21,13 @@
             <div id="botTipBox" class="botTipBox">
                 <p class="p2"><i style="margin-right:2px; vertical-align:middle" class="uicon uicon-pft"></i><span class="t">票付通-技术支持</span></p>
             </div>
+            <div class="dialog_mask" v-show="{{dialog_status}}"></div>
+            <div class="dialog_con"  v-show="{{dialog_status}}">fsdfsd</div>
         </div>
     </div>
 </template>
-<script type="es6">
+<!--<script type="es6">-->
+<script>
     const Login = require("SERVICE_M/mall-member-smslogin");
     const VCODE_INTER_TIME = 60; //验证码获取时间间隔60s
     let timer = null;
@@ -34,8 +37,10 @@
                 mobile : "",
                 code : "",
                 time : -1,
-                vcodeState : ""
-            }
+                vcodeState : "",
+                dialog_status:"show"
+            };
+
         },
         props : {
             to : {
@@ -60,14 +65,59 @@
             }
         },
         methods : {
+            onMobileInpBlur(){
+                console.log("fsdfsf");
+                var Alert = this.Alert;
+                var mobile = this.mobile;
+                if(!mobile) return Alert("请填写手机号");
+                if(!PFT.Util.Validate.typePhone(mobile)) {
+                    $("#getCheckMaBtn").addClass("disable")
+                    return Alert("手机号格式有误")
+                };
+                $.ajax({
+                url: "/r/Mall_Member/isAvalidAccount/",    //请求的url地址
+                dataType: "json",   //返回格式为json
+                async: true, //请求是否异步，默认为异步，这也是ajax重要特性
+                data: {
+                    "mobile": mobile,
+                    token : PFT.Util.getToken()
+                },    //参数值
+                type: "post",   //请求方式
+                timeout:10000,   //设置超时 10000毫秒
+                beforeSend: function() {
+                    //请求前的处理
+                },
+                success: function(res) {
+                    //请求成功时处理
+                    if(res.code==200){
+                        if(res.data.avalid==1){
+                            $("#getCheckMaBtn").removeClass("disable")
+                        }else{
+                            $("#getCheckMaBtn").addClass("disable")
+                        }
+                    }else{
+                        Alert(res.msg)
+                    }
+                },
+                complete: function(res,status) {
+                    //请求完成的处理
+                    if(status=="timeout"){
+                        alert("请求超时")
+                    }
+                },
+                error: function() {
+                    //请求出错处理
+                }
+            });
+            },
             onSubmit(e){
                 var submitBtn = e.target;
                 var Alert = this.Alert;
                 if(submitBtn.classList.contains("disable")) return false;
-                var mobile = this.mobile;
+//                var mobile = this.mobile;
                 var code = this.code;
-                if(!mobile) return Alert("请填写手机号");
-                if(!PFT.Util.Validate.typePhone(mobile)) return Alert("手机号格式有误");
+//                if(!mobile) return Alert("请填写手机号");
+//                if(!PFT.Util.Validate.typePhone(mobile)) return Alert("手机号格式有误");
                 if(!code) return Alert("请填写验证码");
                 if(isNaN(code) || code.length!==6) return Alert("请填写6位数数字验证码");
                 Login({mobile:mobile,code:code},{
@@ -165,5 +215,14 @@
     .loginPage .topBtn:active{ background:rgba(0,0,0,0.5)}
 
     .loginPage .loginTip{ color:#fff; padding:0 10px 8px 5px; font-size:12px}
+    .dialog_mask{
+        height: 100%;
+        width: 100%;
+        background-color: black;
+        opacity: 0.5;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
 
 </style>
