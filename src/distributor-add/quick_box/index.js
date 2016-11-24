6be8681ca99205e5b749f749e5d8ce64d0f742cf
zhengjiashen <122760248@qq.com>
2016-11-24 18:08:36 +0800

    添加分销商页面前后端分离第一个版本提交

--- src/distributor-add/quick_box/index.js
+++ src/distributor-add/quick_box/index.js
@@ -29,8 +29,12 @@ var QUICK_BOX=PFT.Util.Class({
         $.post("../call/jh_mem.php",
             {action:"chkAndGetDname",dname:$(e.target).attr("data-name")},
             function (req){
-                var fakedata = {"id":"1929","dname":"\u5fae\u5929\u4e0b\uff08\u53a6\u95e8\uff09\u7f51\u7edc\u79d1\u6280\u6709\u9650\u516c\u53f8","mobile":"15859107801","passport":"200766","cname":"\u90b9\u5efa\u534e","com_type":"\u65c5\u884c\u793e","created":0,"ajaxStatus":2,"ajaxMsg":"\u5b58\u5728\u8be5\u8d26\u53f7","ajaxMobile":"15859107801"}
-                _this.trigger("quickShow",fakedata)
+                if(req.ajaxStatus == "fail"){
+                    alert(req.reqMsg);
+                    return false
+                }
+                //var fakedata = {"id":"1929","dname":"\u5fae\u5929\u4e0b\uff08\u53a6\u95e8\uff09\u7f51\u7edc\u79d1\u6280\u6709\u9650\u516c\u53f8","mobile":"15859107801","passport":"200766","cname":"\u90b9\u5efa\u534e","com_type":"\u65c5\u884c\u793e","created":0,"ajaxStatus":2,"ajaxMsg":"\u5b58\u5728\u8be5\u8d26\u53f7","ajaxMobile":"15859107801"}
+                _this.trigger("quickShow",req)
             },"json")
     },
 
