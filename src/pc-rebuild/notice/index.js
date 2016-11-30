59ae4a9695ecb845d0bebace888a73c08afa91f5
zhengjiashen <122760248@qq.com>
2016-11-30 15:24:29 +0800

    暂存

--- src/pc-rebuild/notice/index.js
+++ src/pc-rebuild/notice/index.js
@@ -3,41 +3,19 @@
  * Date: 2016/11/30 10:10
  * Description: ""
  */
+var notice_tpl = require("./notice_tpl.xtpl");
 
-var notice_tpl = "./notice_tpl.xtpl";
 //块级写法：
-var newPart1=PFT.Util.Class({
-    //放入容器
-    container:"#system-notice",
+var system_notice = {
+    init:function () {
 
-//绑定事件
-    EVENTS:{
-        // "click #id1":"event1",
-        // "click .class1":"event2"
-    },
-
-    //init()方法在实例化以后会默认执行
-    init:function(){
-        console.log(2)
-        $("#system-notice").append(notice_tpl);
-        console.log(1)
-    },
-
-    //事件调用方法1
-    event1:function(){
-
-        this.selfFunctionCall1()   //可以调用自身的函数
-        this.trigger("customizedEvent1",data)   //可以发布自定义事件用于在主模块监听
-
-    },
-//事件调用方法2
-event2:function(){
-},
-
-//用于函数内部自我调用的方法
-selfFunctionCall1:function(){
+        $("#system-notice").append(notice_tpl)
+    }
 }
-});
+
+window.onload = function () {
+    system_notice.init()
+}
 
 //模块导出
-module.exports=newPart1;
+module.exports=system_notice;
