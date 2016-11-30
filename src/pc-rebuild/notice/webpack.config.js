0412163db124c365067e3e4b7077bb9ffa1d475b
zhengjiashen <122760248@qq.com>
2016-11-30 14:55:45 +0800

    暂存

--- src/pc-rebuild/notice/webpack.config.js
+++ src/pc-rebuild/notice/webpack.config.js
@@ -3,11 +3,11 @@
  * Date: 2016/7/11 11:17
  * Description: ""
  */
-var env = require("../../task-webpack/getNodeENV");
+var env = require("../../../task-webpack/getNodeENV");
 var path = require("path");
-var plugins = require("../../task-webpack/getPlugins")(env);
-var output = require("../../task-webpack/getOutput")(env);
-var config = require("../../task-webpack/config")({
+var plugins = require("../../../task-webpack/getPlugins")(env);
+var output = require("../../../task-webpack/getOutput")(env);
+var config = require("../../../task-webpack/config")({
 	entry : {
 		//平台-前后端分离-系统通知页
 		"system_notice.pc" : "./src/pc-rebuild/notice/index.js"
