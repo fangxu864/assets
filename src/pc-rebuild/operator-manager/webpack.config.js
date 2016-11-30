be9aae5a85d588cd652779d485a76556adad9b01
huangzy <huangzhiyang166@gmail.com>
2016-11-30 10:31:07 +0800

    平台平后端分离项目-员工管理模块-包含2个页面：员工列表页及操作日志页

--- src/pc-rebuild/operator-manager/webpack.config.js
+++ src/pc-rebuild/operator-manager/webpack.config.js
@@ -9,8 +9,10 @@ var plugins = require("../../task-webpack/getPlugins")(env);
 var output = require("../../task-webpack/getOutput")(env);
 var config = require("../../task-webpack/config")({
 	entry : {
-		//平台-前后端分离-系统通知页
-		"system_notice.pc" : "./src/pc-rebuild/notice/index.js"
+		//平台-前后端分离-员工管理-列表页
+		"operator.manager.list" : "./src/pc-rebuild/operator-manager/list/index.js",
+		//平台-前后端分离-员工管理-操作日志页
+		"operator.manager.record" : "./src/pc-rebuild/operator-manager/record/index.js"
 	},
 	output : output,
 	plugins : plugins
