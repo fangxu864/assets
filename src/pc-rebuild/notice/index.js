fee4efb0e5b37b87ceb16f7fe2e6853cf07a0808
zhengjiashen <122760248@qq.com>
2016-12-01 18:25:07 +0800

    基本框架完成，还差数据缓存部分

--- src/pc-rebuild/notice/index.js
+++ src/pc-rebuild/notice/index.js
@@ -8,52 +8,100 @@ require("./ajax.js");
 var notice_tpl = require("./notice_tpl.xtpl");
 var CHANGE_PAGE = require("./pagination_box/index.js");
 var MESSAGE_BOX = require("./message_box/index.js");
+var message_send_tpl = require("./message_box/message_send.xtpl");
 
-var Main={
-    //初始化
-    init:function () {
+var Main= {
+        //初始化
+        init: function () {
 
-        var _this = this;
+            var _this = this;
 
-        //导入基础模版
-        $(".system-notice").append(notice_tpl);
+            //导入基础模版
+            $(".system-notice").append(notice_tpl);
 
-        //实例化各个模块
-        _this.pagination = new CHANGE_PAGE();
-        _this.message_box = new MESSAGE_BOX();
+            //实例化各个模块
+            _this.pagination = new CHANGE_PAGE();
+            _this.message_box = new MESSAGE_BOX();
 
 
-        //缓存后台数据
-        _this.ajax = ajax;
-        _this.ajax({
-            url:"#",
-            method:"POST",
-            data:null
-        }).then(function (xhr) {
+            //缓存后台数据
+            var fakeData = [{content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+                {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"}
+            ];
 
-        }).catch(function (error) {
-            console.log(error.errorType)
-        })
+            var afterDevide =_this.dividePage(fakeData,5);
+            var Page1 = afterDevide.cacheData[0];
 
-        //分页器渲染
-        _this.pagination.render(1,5);
+            $("#message_container").empty();
+            $("#message_container").append(message_send_tpl);
+            //加载数据
+            for(var i = 0 ; i<Page1.length ; i++){
+                var li = $("<li> <input type='checkbox'/> <span class='blue'>"+Page1[i].content+"</span> <span class='time'>"+Page1[i].time+"</span> </li>");
+                $(".message_send ul").append(li);
+            }
+            //分页器渲染
+            _this.pagination.render(1, afterDevide.totalPage);
 
 
 //运行模块交流部分
-        this.CommunicateArea()
-    },
+            this.CommunicateArea()
+        },
 
 //所有模块的交流区域
-    CommunicateArea:function(){
+        CommunicateArea: function () {
+            var _this = this;
+            _this.message_box.on("changePage_send",function (page) {
+                alert(page)
+            })
+        },
 
-    },
+        dividePage: function (data, size) {
 
-    PublicFuction:{
-        Function1:function(){}
+            //求出数据分页后的总页数
+            var whole = data;
+            var length = data.length;
+            var totalPage = Math.ceil(length/parseInt(size));
 
-}
 
-}
+            var cacheData = [];
+            for(var i=0 ; i<totalPage ; i++){
+                var part = [];
+                for(var j=0 ; j<size ; j++){
+                    if(whole[j]){
+                        part.push(whole[j]);
+                    }
+                }
+
+                //如果加一个删一个就会每次漏掉一个，所以要在循环外面删除
+                whole.splice(0,size);
+
+                cacheData.push(part)
+            }
+            return {cacheData:cacheData,totalPage:totalPage}
+        }
+    }
+;
 
 
 $(function () {
@@ -61,5 +109,3 @@ $(function () {
 });
 
 
-//模块导出
-//module.exports=system_notice;
--- src/pc-rebuild/notice/message_box/index.js
+++ src/pc-rebuild/notice/message_box/index.js
@@ -19,43 +19,130 @@ var message_box=PFT.Util.Class({
         "click #message-all":"btn_all",
         "click #message-unread":"btn_unread",
         "click #message-read":"btn_read",
+
+        //全选 标记为已读 删除
+        "click #chooseAll":"btn_chooseAll",
+        "click #mark":"btn_mark",
+        "click #delete":"btn_delete"
     },
 
     //init()方法在实例化以后会默认执行
     init:function(){
+        var _this = this;
         $("#message_container").empty();
         $("#message_container").append(message_send_tpl);
     },
+    
+    //初始化部分不能发布事件，因为顺序问题不会得到相应
 
 
     //内部事件
         //已发送通知 和 收到通知
     btn_send:function (e) {
+        //动画
         $(e.target).addClass("active_tab").siblings().removeClass("active_tab");
 
+        //清空数据，加载新模版
         $("#message_container").empty();
         $("#message_container").append(message_send_tpl);
+
+        //假数据
+        var fakeData = [{content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"}
+        ];
+        var req = fakeData;
+
+        //加载数据
+        for(var i = 0 ; i<fakeData.length ; i++){
+            var li = $("<li> <input type='checkbox'/> <span class='blue'>"+req[i].content+"</span> <span class='time'>"+req[i].time+"</span> </li>");
+            $(".message_send ul").append(li);
+        }
+
     },
 
     btn_accept:function (e) {
         $(e.target).addClass("active_tab").siblings().removeClass("active_tab");
 
+        //清空
         $("#message_container").empty();
         $("#message_container").append(message_accept_tpl);
+
+        //假数据
+        var fakeData = [{status:"[未读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[未读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[已读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[未读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[未读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[已读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[未读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[未读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[已读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+            {status:"[未读]",content:"[2016-05-31]测试发布通知咯咯",time:"2016-05-31"},
+
+        ];
+
+        var req = fakeData;
+        //加载数据
+        for(var i = 0 ; i<fakeData.length ; i++){
+            var li = $("<li> <input type='checkbox'/> <span>"+req[i].status+"</span> <span>"+req[i].content+"</span> <span class='time'>"+req[i].content+"</span> </li>");
+            $(".message_accept ul:eq(1)").append(li);
+        }
     },
 
         //已发送通知 和 收到通知
     btn_all:function (e) {
         $(e.target).addClass("active_type").siblings().removeClass("active_type");
+        $(".message_accept ul:eq(1)").empty()
+
     },
     btn_unread:function (e) {
         $(e.target).addClass("active_type").siblings().removeClass("active_type");
+        $(".message_accept ul:eq(1)").empty()
     },
     btn_read:function (e) {
         $(e.target).addClass("active_type").siblings().removeClass("active_type");
+        $(".message_accept ul:eq(1)").empty()
     },
 
-        //
+        //全选 标记为已读 删除
+    btn_chooseAll:function () {
+        var isChoose = [];
+
+        $(".content").find("li").each(function (index,Element) {
+            if($(Element).find("input[type='checkbox']").attr("checked")){
+                isChoose.push(1)
+            }else{
+                isChoose.push(0)
+            }
+        });
+
+        var num = 0 ;
+        var flag = 0 ;
+        for(var i = 0 ; i < isChoose.length ; i++){
+            num += isChoose[i];
+            flag += 1;
+        }
+
+        if(num == flag){
+            $(".content").find("li").each(function (index,Element) {
+                $(Element).find("input[type='checkbox']").attr("checked",false)
+            })
+        }else{
+            $(".content").find("li").each(function (index,Element) {
+                $(Element).find("input[type='checkbox']").attr("checked",true)
+            })
+        }
+
+
+    },
 
 
 
