799f7d70594a721e39f7042141cdfadd3069326e
huangzy <huangzhiyang166@gmail.com>
2016-12-01 11:00:18 +0800

    应用中心项目初始化

---
+++ src/application-center/index/index.js
@@ -0,0 +1,32 @@
+/**
+ * Author: huangzhiyang
+ * Date: 2016/12/1 9:52
+ * Description: ""
+ */
+require("./scss/app-item.scss");
+var Template = {
+	appBox : PFT.Util.ParseTemplate(require("./tpl/app-box.tpl"))
+};
+var Main = PFT.Util.Class({
+	init : function(){
+		this.renderAppBox({
+			id : "121",
+			name : "微商城",
+			iconCls : "sMall",
+			priceText : ""
+
+		})
+
+
+
+
+
+
+
+
+	},
+	renderAppBox : function(data){
+		var html = Template.appBox(data);
+		console.log(html);
+	}
+});
