c751848284ae9c101069e8d36fb11d4f12b5f002
huangzy <huangzhiyang166@gmail.com>
2016-12-05 15:59:46 +0800

    no message

--- common/webpack.config.pft.common.pc.style.js
+++ common/webpack.config.pft.common.pc.style.js
@@ -5,12 +5,11 @@
  */
 var env = require("../task-webpack/getNodeENV");
 var path = require("path");
-var plugins = require("../task-webpack/getPlugins")(env);
-var output = require("../task-webpack/getOutput")(env,"all");
+var plugins = require("../task-webpack/getPlugins")(env,"[name].all","v2.0");
+var output = require("../task-webpack/getOutput")(env,"all","v2.0");
 var config = require("../task-webpack/config")({
 	entry : {
-		"pft.common.mb" : "./common/pft.common.mb.js",   //use in mobile
-		"pft.common.pc" : "./common/pft.common.pc.js"    //use in pc
+		"pft.common.pc" : "./common/pft.common.pc.style.js"    //use in pc
 	},
 	output : output,
 	plugins : plugins
