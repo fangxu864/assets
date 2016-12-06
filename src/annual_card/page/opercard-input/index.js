8612a97cdd2fb1f6ee9ae0cf57b78e0200556890
WangTong <895522268@qq.com>
2016-12-06 10:06:49 +0800

    修改添加员工卡号功能JS

--- src/annual_card/page/opercard-input/index.js
+++ src/annual_card/page/opercard-input/index.js
@@ -1,14 +1,14 @@
 
 require("./index.scss");
-var tpl= require("./index.xtpl");
+var tpl = require("./index.xtpl");
 var Api = require("../../common/api.js");
-var ReadPhysicsCard =require("../../common/ReadPhysicsCard.js")
+var ReadPhysicsCard = require("../../common/ReadPhysicsCard.js")
 $(".inpNum").after(tpl);
 var MainView = Backbone.View.extend({
     el: $("form"),
     events: {
         "click input[type='submit']": "onAddBtnClick",
-        "change .inpNum": "onTextInpChange",
+
         "keydown .inpNum": "onCardInpKeyDown",
         "click #clearCardInpBtn": "onClearCardInpBtnClick"
     },
@@ -16,48 +16,85 @@ var MainView = Backbone.View.extend({
         var that = this;
         this.submitBtn = $(".btn>.inpbtn[type='submit']");
         this.ReadPhysicsCard = new ReadPhysicsCard({ id: "readCardObj" });
-        this.cardInp=$(".inpNum");
-        this.phyNo=$("input[name='physics_no']");
-        this.visiNo=$("input[name='visible_no']");
+        this.cardInp = $(".inpNum");
+        this.phyNo = $("input[name='physics_no']");
+        this.visiNo = $("input[name='visible_no']");
     },
     onAddBtnClick: function (e) {
         //var InpVal = this.cardInp.val();      
-        var that =this;
-        var phyVal=that.phyNo.val();
-        var visiVal=that.visiNo.val();
-        if (phyVal==="") { 
-            alert("物理卡号不能为空"); 
-            that.phyNo.focus();
-            return false;
-        }else if(visiVal===""){
-            alert("实体卡号不能为空");
-            that.visiNo.focus();
-            return false;       
-        }
-    },
-    onTextInpChange: function (e) {
         var that = this;
-        var tarInp = $(e.currentTarget);
-        var val = tarInp.val();
-        if (val==="") return alert("物理卡号和实体卡号不能为空"); ;
-        
+        var phyVal = that.phyNo.val();
+        var visiVal = that.visiNo.val();
+        var reg = /^[A-Za-z0-9]+$/;
+        console.log((reg.test(visiVal) + "+" + reg.test(phyVal)));
+        if (phyVal === "" && visiVal !== "") {
+            alert("物理卡号不能为空");
+            if (!reg.test(visiVal)) {
+                alert("实体卡号仅允许字母与数字,不能有空格");
+                that.phyNo.focus();
+                return false;
+            }
+            return false;
+        } else if (visiVal === "" && phyVal !== "") {
+            alert("实体卡号不能为空");
+            if (!reg.test(phyVal)) {
+                console.log((reg.test(visiVal) + "+" + reg.test(phyVal)));
+                alert("物理卡号仅允许字母与数字,不能有空格");
+                that.visiNo.focus();
+                return false;
+            }
+            return false;
+        } else {
+            if (!reg.test(phyVal) && phyVal !== "") {
+                alert("物理卡号仅允许字母与数字,不能有空格");
+                that.phyNo.focus();
+                return false;
+            } else if (!reg.test(visiVal) && visiVal !== "") {
+                console.log((reg.test(visiVal) + "+" + reg.test(phyVal)));
+                alert("实体卡号仅允许字母与数字,不能有空格");
+                that.visiNo.focus();
+                return false;
+            }
+            else if (!reg.test(phyVal) && !reg.test(visiVal) || reg.test(phyVal && reg.test(visiVal))) {
+                if (phyVal === "" && visiVal !== "") {
+                    alert("物理卡号不能为空");
+                    if (!reg.test(visiVal) && visiVal !== "") {
+                        console.log((reg.test(visiVal) + "+" + reg.test(phyVal)));
+                        alert("实体卡号仅允许字母与数字,不能有空格");
+                        that.phyNo.focus();
+                        return false;
+                    }
+                    return false;
+                } else if (visiVal === "" && phyVal !== "") {
+                    alert("实体卡号不能为空");
+                    if (!reg.test(phyVal) && phyVal !== "") {
+                        alert("物理卡号仅允许字母与数字,不能有空格");
+                        that.visiNo.focus();
+                        return false;
+                    }
+                    return false;
+                } else if (visiVal === "" && phyVal === "") {
+                    return true;
+                }
+            }
+        };
     },
+
     onClearCardInpBtnClick: function (e) {
-      var clearBtn = $(e.currentTarget);
-      var txtInp=clearBtn.parents(".rt").prev();
-        txtInp.val("").focus();     
+        var clearBtn = $(e.currentTarget);
+        var txtInp = clearBtn.parents(".rt").prev();
+        txtInp.val("").focus();
         clearBtn.hide();
     },
     onCardInpKeyDown: function (e) {
         var tarInp = $(e.currentTarget);
-        var clearBtn=tarInp.next().find(".clearCardInpBtn");
+        var clearBtn = tarInp.next().find(".clearCardInpBtn");
         var val = tarInp.val();
         var keycode = e.keyCode;
-        val ? clearBtn.show() : clearBtn.hide();      
+        val ? clearBtn.show() : clearBtn.hide();
         if (keycode == 13) return false;
     }
-    
 })
-$(function(){
-  new MainView();
+$(function () {
+    new MainView();
 })
