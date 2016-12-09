15fb6de064a80feaae84d94569380d5fda99cdb1
WangTong <895522268@qq.com>
2016-12-09 14:41:37 +0800

    资质认证组件修改

--- src/newcertifi/index.js
+++ src/newcertifi/index.js
@@ -1,7 +1,7 @@
 
 var tpl=require("./index.xtpl");
 
-$(document).ready(function(){ 
+
 $.ajax({
     url:'/r/Member_MemberCertifi/getCertifiInfo',
     type:'POST', //GET
@@ -9,30 +9,24 @@ $.ajax({
     timeout:5000,    //超时时间
     dataType:'json',    //返回的数据格式：json/xml/html/script/jsonp/text
     beforeSend:function(xhr){
-        console.log(xhr)
-        console.log('发送前')
+        
     },
     success:function(res){
         var list=res.data;
-        if(res.code===200){
-           
+        if(res.code===200){        
                 var render = PFT.Util.ParseTemplate(tpl);
                 var html = render({list:list});
                 $("#memCertifyContainer").append(html);
-            
         }
     },
     error:function(xhr,textStatus){
-        console.log('错误')
-        console.log(xhr)
-        console.log(textStatus)
+
     },
     complete:function(){
-        console.log('结束')
+       
     }
 })
 
-    })
 
 
 
