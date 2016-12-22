218c4648064aa42a7388103ac52c0c2cdea32663
moderations <40919157@qq.com>
2016-12-22 11:29:25 +0800

    asd

--- src/application-center/webpack.config.js
+++ src/application-center/webpack.config.js
@@ -9,8 +9,8 @@ var plugins = require("../../task-webpack/getPlugins")(env);
 var output = require("../../task-webpack/getOutput")(env);
 var config = require("../../task-webpack/config")({
 	entry : {
-	//应用中心 - 首页
-	"appcenter_index" : "./src/application-center/index/index.js",
+    	//应用中心 - 首页
+    	"appcenter_index" : "./src/application-center/index/index.js",
         //应用中心 - 详情
         "appcenter_details" : "./src/application-center/details/index.js",
         //应用中心 - 应用费用配置列表
