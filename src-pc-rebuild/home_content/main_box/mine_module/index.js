cbca38f66833cc0d50a85cef5e7cc1acc89afb88
zhengjiashen <122760248@qq.com>
2016-12-30 14:23:45 +0800

    暂存

--- src-pc-rebuild/home_content/main_box/mine_module/index.js
+++ src-pc-rebuild/home_content/main_box/mine_module/index.js
@@ -6,14 +6,27 @@ var Class = require("COMMON/js/util.class.js");
 //块级写法：
 var mine_module=Class({
     //放入容器
-    container:"#mine_module",
+    container:"#main_box",
 
 //绑定事件
-    EVENTS:{},
+    EVENTS:{
+    },
     init:function(){
         console.log("mine load sucessfully");
-         $("#main_box").append(mine_xtpl)
+         $("#main_box").append(mine_xtpl);
+         this.hidePersonalImg()
     },
+    hidePersonalImg:function () {
+        window.onresize = function () {
+            var width = $("#mine_module").width();
+            if(width < 688){
+                $("#personalImg").hide();
+            }
+            if(width >= 688){
+                $("#personalImg").show();
+            }
+        }
+    }
 });
 
 //模块导出
