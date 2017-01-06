eab5b9aa829cf244c03e46b80f565c7c313572c4
huangzy <huangzhiyang166@gmail.com>
2017-01-06 13:29:35 +0800

    no message

--- wxapp-mall/pages/ordercenter/ordercenter.js
+++ wxapp-mall/pages/ordercenter/ordercenter.js
@@ -2,7 +2,7 @@ var Common = require("../../utils/common.js");
 var app = getApp();
 Page({
     data: {
-
+		showRefreshLoading : true
     },
 	onReady : function(){
 		wx.setNavigationBarTitle({title:"我的订单"})
