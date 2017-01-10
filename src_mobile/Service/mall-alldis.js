79d304b282a018fe92e73386bbe5124fb97a53d7
huangzy <huangzhiyang166@gmail.com>
2017-01-10 16:54:13 +0800

    no message

--- src_mobile/Service/mall-alldis.js
+++ src_mobile/Service/mall-alldis.js
@@ -37,3 +37,110 @@ exports.getStatus = function(opt){
 		}
 	})
 }
+
+//获取全民分销数据列表
+exports.getList = function(opt){
+	opt = PFT.Util.Mixin(PFT.Config.Ajax(),opt);
+
+
+	if(opt.debug){
+		opt.loading();
+		setTimeout(function(){
+			opt.complete();
+			opt.success({
+				"nickname": "慢慢",  // 昵称
+				"head_img": "xxxx.img", // 头像
+				"sumComm": 0.49, // 总佣金
+				"dis_list": [
+					{
+						"nickname": "111",  // 昵称
+						"order_count": "3", // 订单数量
+						"develop_money": 0, // 发展佣金
+						"recommend_money": 2.47 // 介绍佣金
+					},
+					{
+						"nickname": "111",  // 昵称
+						"order_count": "3", // 订单数量
+						"develop_money": 0, // 发展佣金
+						"recommend_money": 2.47 // 介绍佣金
+					},
+					{
+						"nickname": "111",  // 昵称
+						"order_count": "3", // 订单数量
+						"develop_money": 0, // 发展佣金
+						"recommend_money": 2.47 // 介绍佣金
+					},
+					{
+						"nickname": "111",  // 昵称
+						"order_count": "3", // 订单数量
+						"develop_money": 0, // 发展佣金
+						"recommend_money": 2.47 // 介绍佣金
+					}
+				]
+			})
+		},1000);
+		return false;
+	}
+
+
+	PFT.Util.Ajax(api("getAllDisCenterData"),{
+		type : "post",
+		params : {
+			token : PFT.Util.getToken()
+		},
+		loading : opt.loading,
+		complete : opt.complete,
+		success : function(res){
+			res = res || {};
+			var code = res.code;
+			var msg = res.msg || PFT.AJAX_ERROR_TEXT;
+			var data = res.data;
+			if(code==200){
+				opt.success(data);
+			}else{
+				opt.fail(msg,code)
+			}
+		},
+		serverError : function(){
+			alert(PFT.AJAX_ERROR_TEXT);
+		}
+	})
+}
+
+
+//提现佣金到红包
+exports.cash = function(opt){
+	opt = PFT.Util.Mixin(PFT.Config.Ajax(),opt);
+
+
+	if(opt.debug){
+		opt.loading();
+		setTimeout(function(){
+			opt.complete();
+			opt.success({});
+		},1000)
+		return false;
+	}
+
+	PFT.Util.Ajax(api("cash"),{
+		type : "method",
+		data : {
+			money : money,
+			token : PFT.Util.getToken()
+		},
+		loading : opt.loading,
+		complete : opt.complete,
+		success : function(res){
+			var code = res.code;
+			if(code==200){
+				opt.success(data);
+			}else{
+				opt.fail(res.msg,code);
+			}
+		},
+		serverError : function(){
+			alert(PFT.AJAX_ERROR_TEXT);
+		}
+	})
+
+}
