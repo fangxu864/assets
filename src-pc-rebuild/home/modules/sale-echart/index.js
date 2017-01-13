77bbbe04bd1cb1b1760d8229d550e1e2b34889fa
huangzy <huangzhiyang166@gmail.com>
2017-01-13 11:33:12 +0800

    no message

---
+++ src-pc-rebuild/home/modules/sale-echart/index.js
@@ -0,0 +1,5 @@
+/**
+ * Author: huangzhiyang
+ * Date: 2017/1/13 11:32
+ * Description: ""
+ */
--- src-pc-rebuild/home/modules/userinfo/index.js
+++ src-pc-rebuild/home/modules/userinfo/index.js
@@ -17,7 +17,18 @@ module.exports = function(parent){
 			this.render();
 		},
 		render : function(data){
-			var html = this.template(data || {});
+			var html = this.template(data || {
+					"name": "慢慢的店铺",   //账号名称
+					"cname" : "天地银行",   //公司名称
+					"type" : 1, //账号类型,0供应商,1分销商
+					'hasAuth' : 1, //是否认证
+					"remainMoney": 111,     //余额
+					"expireDate" : "2012-1-1",   //到期时间
+					"mobile": 123123123123,      //手机号
+					"abnormalOrder" : 12,    //异常订单  ,
+					"lastLogin" : "2012-1-1", //上次登陆时间
+					"avatar" : "http://images.12301.cc/123624/1452148699.png" //头像
+				});
 			this.container.html(html);
 		}
 	});
