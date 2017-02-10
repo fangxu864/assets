655e9376637355a66cfc1d728974e4c2ac236712
houxingyi <1026043046@qq.com>
2017-02-10 09:38:44 +0800

    过期应用提醒

--- src/application-center/expiredText/index.js
+++ src/application-center/expiredText/index.js
@@ -7,6 +7,7 @@
    var dialog=require("COMMON/modules/dialog-simple");
    var expiredTextTpl = require("./expiredText.tpl");
    var memberID = $("#memberId").attr("data-id");
+
    //过期模拟部分————————————————————————————————————————————————————————
    var dialog_expired = new dialog({
        width : 800,
@@ -46,20 +47,28 @@
          var code = res.code;
 
          if(code==200){
-             dialog_expired.open();
-             $.each(data,function (index,value) {
-                 var i = index+1;
-                 if(index < 10){
-                     var seq = "0"+i;
-                 }else {
-                     var seq = i;
-                 }
-                 var newLi = $('<li> <span>'+seq+'、</span> <span> <em style="padding: 0 5px">'+value[0].name+'</em>将于<em class="etime">'+value[0].expire_time+'</em>到期。</span> <span class="renew" data-id="'+value[0].module_id+'">续费</span> </li>')
-                 $("#appBox_expired").append(newLi)
-             });
-             $(".renew").on("click",function (e) {
-                 window.location.href="new/appcenter_pay.html?appid="+$(e.target).attr("data-id");
-             })
+
+             if(data.length > 0){
+
+               dialog_expired.open();
+               $.each(data,function (index,value) {
+                   var i = index+1;
+                   if(index < 10){
+                       var seq = "0"+i;
+                   }else {
+                       var seq = i;
+                   }
+                   var newLi = $('<li> <span>'+seq+'、</span> <span> <em style="padding: 0 5px">'+value[0].name+'</em>将于<em class="etime">'+value[0].expire_time+'</em>到期。</span> <span class="renew" data-id="'+value[0].module_id+'">续费</span> </li>')
+                   $("#appBox_expired").append(newLi)
+               });
+               $(".renew").on("click",function (e) {
+                   window.location.href="new/appcenter_pay.html?appid="+$(e.target).attr("data-id");
+               })  
+
+             }else{
+              console.log("无过期应用");
+             } 
+             
          }else{
              console.log(res.msg)
          }
