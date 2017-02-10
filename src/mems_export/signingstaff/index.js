46ebb0efe6eee4842622633f7765829a29e3a4df
fangxu <864109504@qq.com>
2017-02-10 10:55:40 +0800

    s

--- src/mems_export/signingstaff/index.js
+++ src/mems_export/signingstaff/index.js
@@ -1,15 +1,16 @@
 /**
  * Created by Administrator on 2017/2/9.
  */
-
+require("./index.scss");
 var ParseTemplate = require("COMMON/js/util.parseTemplate");
 var Dialog=require("COMMON/modules/dialog-simple");
+var tpl = require("./index.xtpl");
 
 var Dial=new Dialog({
     width : 700,
     height : 600,
     closeBtn : true,
-    content : "123456",
+    content : tpl,
     drag : true,
     speed : 100,
     onCloseAfter : function(){
