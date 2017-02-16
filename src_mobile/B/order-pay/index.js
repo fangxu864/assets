3d2d057b1346e1e948f1bd4b03a233102dc89472
WangTong <895522268@qq.com>
2017-02-16 09:37:41 +0800

    增加pay页面样式

--- src_mobile/B/order-pay/index.js
+++ src_mobile/B/order-pay/index.js
@@ -2,15 +2,17 @@
 require("./index.scss");
 
 var Order_pay = PFT.Util.Class({
-
 	container : $("#orderPayBox"),
+	init : function(opt){         
+		console.log("支付");
+		var _this=this;
+	},
+	params :{
+		token:PFT.Util.getToken(),
+
+	},
 	EVENTS : {                    
 	},
-	init : function(opt){         
-		
-		console.log("支付");
-		
-	}
 
 });
 
