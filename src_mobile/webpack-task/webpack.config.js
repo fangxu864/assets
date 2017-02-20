87923ebfa274b70bcb17b53564321f45f5109f9c
houxingyi <1026043046@qq.com>
2017-02-20 15:31:10 +0800

    继续填写订单

--- src_mobile/webpack-task/webpack.config.js
+++ src_mobile/webpack-task/webpack.config.js
@@ -13,15 +13,14 @@ var config = require("../../task-webpack/config")({
 	entry : {
 
 
-		//"mb.plist.b" : path.resolve(ROOT_URL,"B/plist/index.js"),
 
 		// "mb.plist.b" : path.resolve(ROOT_URL,"B/plist/index.js"),
 
 		// "mb.product_detail.b" : path.resolve(ROOT_URL,"B/product-detail/index.js"),
 		// "mb.write_order_spot.b" : path.resolve(ROOT_URL,"B/write-order-spot/index.js"),
-		"mb.order_pay.b" : path.resolve(ROOT_URL,"B/order-pay/index.js"),
-		"mb.order_pay_success.b":path.resolve(ROOT_URL,"B/order-pay/ordersuccess/index.js"),
-		//"mb.order_fill.b" : path.resolve(ROOT_URL,"B/order-fill/index.js"),
+		// "mb.order_pay.b" : path.resolve(ROOT_URL,"B/order-pay/index.js"),
+		// "mb.order_pay_success.b":path.resolve(ROOT_URL,"B/order-pay/orderysuccess/index.js"),
+		"mb.order_fill.b" : path.resolve(ROOT_URL,"B/order-fill/index.js"),
 		
 
 		// "mb.index.c" : path.resolve(ROOT_URL,"C/index/index.es6"),
