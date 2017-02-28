0e74fda958fb00c0d4ef232ee89bec30d5ab3108
moderations <40919157@qq.com>
2017-02-28 18:12:57 +0800

    调整cookie设定

--- src/application-center/expiredText/index.js
+++ src/application-center/expiredText/index.js
@@ -26,54 +26,93 @@
      var lastDate = Cookie.getCookie('lastDate'+memberID);
      console.log(lastDate);
      console.log(new Date().getDate());
-     if(lastDate){
-         var today = new Date().getDate();
-         if(today == lastDate){
-             console.log("Cookie存在,但是相等");
-             // return false
-         }else{
-             console.log("Cookie存在,但是不相等");
 
-             Cookie.setCookie('lastDate'+memberID,today,{expireHours:24});
-         }
-     }else{
-         console.log("Cookie不存在");
-         var today = new Date().getDate();
-         Cookie.setCookie('lastDate'+memberID,today,{expireHours:24});
+     var today = new Date().getDate();
+
+     if( !lastDate || today != lastDate ) {
+        // 第一次访问或者隔天访问时 弹窗提示
+        Cookie.setCookie('lastDate'+memberID,today,{expireHours:24});
+
+        $.post("/r/AppCenter_ModuleList/getModuleListOfExpireSoon",{},function (res) {
+           var data = res.data;
+           var code = res.code;
+
+           if(code==200){
+
+               if(data.length > 0){
+
+                 dialog_expired.open();
+                 $.each(data,function (index,value) {
+                     var i = index+1;
+                     if(index < 10){
+                         var seq = "0"+i;
+                     }else {
+                         var seq = i;
+                     }
+                     var newLi = $('<li> <span>'+seq+'、</span> <span> <em style="padding: 0 5px">'+value[0].name+'</em>将于<em class="etime">'+value[0].expire_time+'</em>到期。</span> <span class="renew" data-id="'+value[0].module_id+'">续费</span> </li>')
+                     $("#appBox_expired").append(newLi)
+                 });
+                 $(".renew").on("click",function (e) {
+                     window.location.href="new/appcenter_pay.html?appid="+$(e.target).attr("data-id");
+                 })
+
+               }else{
+                console.log("无过期应用");
+               }
+
+           }else{
+               console.log(res.msg)
+           }
+        })
      }
 
-     $.post("/r/AppCenter_ModuleList/getModuleListOfExpireSoon",{},function (res) {
-         var data = res.data;
-         var code = res.code;
+     // if(lastDate){
+     //     var today = new Date().getDate();
+     //     if(today == lastDate){
+     //         console.log("Cookie存在,但是相等");
+     //         // return false
+     //     }else{
+     //         console.log("Cookie存在,但是不相等");
 
-         if(code==200){
+     //         Cookie.setCookie('lastDate'+memberID,today,{expireHours:24});
+     //     }
+     // }else{
+     //     console.log("Cookie不存在");
+     //     var today = new Date().getDate();
+     //     Cookie.setCookie('lastDate'+memberID,today,{expireHours:24});
+     //     $.post("/r/AppCenter_ModuleList/getModuleListOfExpireSoon",{},function (res) {
+     //         var data = res.data;
+     //         var code = res.code;
 
-             if(data.length > 0){
+     //         if(code==200){
 
-               dialog_expired.open();
-               $.each(data,function (index,value) {
-                   var i = index+1;
-                   if(index < 10){
-                       var seq = "0"+i;
-                   }else {
-                       var seq = i;
-                   }
-                   var newLi = $('<li> <span>'+seq+'、</span> <span> <em style="padding: 0 5px">'+value[0].name+'</em>将于<em class="etime">'+value[0].expire_time+'</em>到期。</span> <span class="renew" data-id="'+value[0].module_id+'">续费</span> </li>')
-                   $("#appBox_expired").append(newLi)
-               });
-               $(".renew").on("click",function (e) {
-                   window.location.href="new/appcenter_pay.html?appid="+$(e.target).attr("data-id");
-               })
+     //             if(data.length > 0){
 
-             }else{
-              console.log("无过期应用");
-             }
+     //               dialog_expired.open();
+     //               $.each(data,function (index,value) {
+     //                   var i = index+1;
+     //                   if(index < 10){
+     //                       var seq = "0"+i;
+     //                   }else {
+     //                       var seq = i;
+     //                   }
+     //                   var newLi = $('<li> <span>'+seq+'、</span> <span> <em style="padding: 0 5px">'+value[0].name+'</em>将于<em class="etime">'+value[0].expire_time+'</em>到期。</span> <span class="renew" data-id="'+value[0].module_id+'">续费</span> </li>')
+     //                   $("#appBox_expired").append(newLi)
+     //               });
+     //               $(".renew").on("click",function (e) {
+     //                   window.location.href="new/appcenter_pay.html?appid="+$(e.target).attr("data-id");
+     //               })
 
-         }else{
-             console.log(res.msg)
-         }
+     //             }else{
+     //              console.log("无过期应用");
+     //             }
 
-     })
+     //         }else{
+     //             console.log(res.msg)
+     //         }
+     //     })
+     // }
+
 
 
 
