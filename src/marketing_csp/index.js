036fcc84c50a7b9205eee5e0340107ee114d5bd5
WangTong <895522268@qq.com>
2017-03-02 17:08:50 +0800

    完善功能

--- src/marketing_csp/index.js
+++ src/marketing_csp/index.js
@@ -16,7 +16,7 @@ var Main = PFT.Util.Class({
 		"click #saveBtn": "saveAddNewActivity"
 	},
 	init: function () {
-		STip("success", "加载成功");
+
 		this.initPage();
 		this.datepicker = new Datepicker();
 
@@ -46,7 +46,7 @@ var Main = PFT.Util.Class({
 
 		if (spid.length < 1) {
 			_this.renderActivityPage(emptyData);
-		//	console.log(emptyData["beginDate"])
+			//	console.log(emptyData["beginDate"])
 		} else {
 			_this.getSpidActivity(spid);
 
@@ -55,15 +55,27 @@ var Main = PFT.Util.Class({
 
 	},
 	//初始化富文本编辑器
-	initUeditor: function (id) {
+	initUeditor: function (id,content,share_type) {
 
 		window.UEDITOR_CONFIG.initialFrameWidth = 600;
 		switch (id) {
 			case 1:
-				this.ue1 = UE.getEditor('content_1');
+				var ue1=this.ue1 = UE.getEditor('content_1');
+				if(content&&share_type==1){
+					ue1.addListener('ready', function () {
+					ue1.setContent(content);  
+				});
+				}
+				
 				break;
 			case 2:
-				this.ue2 = UE.getEditor('content_2');
+				var ue2=this.ue2 = UE.getEditor('content_2');
+				if(content&&share_type==2){
+				ue2.addListener('ready', function () {
+					ue2.setContent(content);  
+				});
+
+				}
 				break;
 		}
 	},
@@ -73,11 +85,11 @@ var Main = PFT.Util.Class({
 		var tarInp = $(e.currentTarget);
 		var CalendarCore = Datepicker.CalendarCore;
 		var date = tarInp.val();
-		var time = tarInp.hasClass("begin") ? "00:00" : "23:00";
+		var time = tarInp.hasClass("begin") ? "00:00" : "23:59";
 		if (!date) {
 			date = CalendarCore.gettoday() + " " + time;
 		}
-		//console.log(date, time)
+		console.log(date, time)
 		datepicker.show(date, {
 			picker: tarInp,              //必选
 			top: 0,                     //可选，相对偏移量
@@ -95,14 +107,14 @@ var Main = PFT.Util.Class({
 			extra: { identify: "coupon" },
 			action: '/r/Resource_ImageUpload/upload',
 			loading: function (formControls) {
-				PFT.Util.STip("success", '<p style="width:160px;">正在上传图片,请稍后</p>', 3000);
+				STip("success", '<p style="width:160px;">正在上传图片,请稍后</p>', 3000);
 			},
 			complete: function (res) {
 				if (res.code == 200) {
-					$('#imagePath').attr({ "src": res.data.src, "data-path": res.data.src }).css({ "width": "240px", "height": "160px" });;
-					PFT.Util.STip("success", '<p style="width:160px;">上传图片成功</p>');
+					$('#imagePath').attr({ "src": res.data.src, "data-path": res.data.src });
+					STip("success", '<p style="width:160px;">上传图片成功</p>');
 				} else {
-					PFT.Util.STip("fail", '<p style="width:220px;">' + res.msg + '</p>');
+					STip("fail", '<p style="width:220px;">' + res.msg + '</p>');
 				}
 			}
 		});
@@ -113,15 +125,15 @@ var Main = PFT.Util.Class({
 			extra: { identify: "coupon" },
 			action: '/r/Resource_ImageUpload/upload',
 			loading: function (formControls) {
-				PFT.Util.STip("success", '<p style="width:160px;">正在上传图片,请稍后</p>', 3000);
+				STip("success", '<p style="width:160px;">正在上传图片,请稍后</p>', 3000);
 			},
 			complete: function (res) {
 
 				if (res.code == 200) {
-					$('#imagePath_2').attr({ "src": res.data.src, "data-path": res.data.src }).css({ "width": "240px", "height": "160px" });
-					PFT.Util.STip("success", '<p style="width:160px;">上传图片成功</p>');
+					$('#imagePath_2').attr({ "src": res.data.src, "data-path": res.data.src })
+					STip("success", '<p style="width:160px;">上传图片成功</p>');
 				} else {
-					PFT.Util.STip("fail", '<p style="width:220px;">' + res.msg + '</p>');
+					STip("fail", '<p style="width:220px;">' + res.msg + '</p>');
 				}
 			}
 		});
@@ -140,12 +152,13 @@ var Main = PFT.Util.Class({
 			success: function (res) {
 				if (res.code == 200) {
 					var data = res.data.data[0];
-					data["spid"]=spid;
+					data["spid"] = spid;
 					_this.renderActivityPage(data);
 
 
 				} else {
-					STip("fail", res.msg);
+					STip("fail", res.msg,2000);
+					window.location.href="marketing_share_list.html";
 				}
 			},
 			fail: function () {
@@ -155,14 +168,15 @@ var Main = PFT.Util.Class({
 	},
 	//时间戳转换
 	formatTimeStamp: function (timestamp) {
-		var date = new Date(timestamp * 1000);
+		var date = new Date(parseInt(timestamp) * 1000 );
 		Y = date.getFullYear() + '-';
 		M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-		D = date.getDate() + ' ';
-		h = date.getHours() + ':';
-		m = date.getMinutes();
+		D = (date.getDate()<10 ? '0'+date.getDate() : date.getDate()) + ' ';
+		h = (date.getHours()<10 ? '0' +date.getHours() : date.getHours())+ ':';
+		m = (date.getMinutes()<10) ? '0' + date.getMinutes() :date.getMinutes();
 
 		var dateTime = Y + M + D + h + m;
+		console.log(dateTime)
 		return dateTime;
 	},
 	//渲染活动页面
@@ -172,17 +186,22 @@ var Main = PFT.Util.Class({
 		var html = "";
 		var beginDate = data.activity_bt ? _this.formatTimeStamp(data.activity_bt) : "";
 		var endDate = data.activity_et ? _this.formatTimeStamp(data.activity_et) : "";
+		var content = data.content;
 		data["beginDate"] = beginDate;
 		data["endDate"] = endDate;
 		html = render({ data: data });
 		_this.container.html(html);
-		_this.initUeditor(1);//初始化默认富文本编辑器
-		_this.initUeditor(2);
+		_this.initUeditor(1,data.content,data.share_type);//初始化默认富文本编辑器
+		_this.initUeditor(2,data.content,data.share_type);
 		_this.getActivityList(data.relation_pid);//初始化活动产品
-		_this.initGetCouponId("couponId",  data.coupon_id);//初始化获取优惠券列表
-		_this.initGetCouponId("couponIdInp",  data.coupon_id);
+		_this.initGetCouponId("couponId", data.coupon_id);//初始化获取优惠券列表
+		_this.initGetCouponId("couponIdInp", data.coupon_id);
+		/*if (data.share_type != 3) {
+			_this.setContent(data.share_type, data.content);//将富文本编辑器内容写入
+		}*/
 		_this.initImgUpload();//初始化图片上传插件
-		$("#saveBtn").attr("data-spid",data.spid);
+
+		$("#saveBtn").attr({ "data-spid": data.spid, "data-mkid": data.mkid });
 		$(" .fileuploadWrap .fileuploadTextInp").css("width", "160px");//重置图片上传input框宽度
 
 	},
@@ -197,7 +216,7 @@ var Main = PFT.Util.Class({
 
 	},
 	//获取产品列表
-	getActivityList: function ( relation_pid) {
+	getActivityList: function (relation_pid) {
 		var _this = this;
 		var select = new Select({
 			source: "/r/product_Coupon/getProducts",
@@ -209,11 +228,11 @@ var Main = PFT.Util.Class({
 			field: {
 				id: "id",
 				name: "title",
-				
+
 			},
 			trigger: $("#schProInp"),
 			filter: false,
-			defaultVal : relation_pid ? relation_pid : "",
+			defaultVal: relation_pid ? relation_pid : "",
 			adaptor: function (res) {
 				$(".selectOptionUl").css("padding-left", 0);
 				var result = { code: 200 };
@@ -221,28 +240,28 @@ var Main = PFT.Util.Class({
 				if (!list) {
 					return result;
 				}
-				$("#schProInp").attr("data-fid",list[0].fid);
+				$("#schProInp").attr("data-fid", list[0].fid);
 				var newList = [];
 				for (var i = 0; i < list.length; i++) {
 
 					newList.push({
 						id: list[i].pid,
 						title: list[i].ltitle,
-						
+
 					});
 				}
 				//console.log(newList)
 				result["data"] = newList;
 
-			//	console.log(share_type)
-				
+				//	console.log(share_type)
+
 				return result;
 			},
 		});
 	},
 	//获取优惠券id 
 	initGetCouponId: function (id, coupon_id) {
-		var _this=this;
+		var _this = this;
 		var selectId = new Select({
 			source: "/r/product_Coupon/getCoupon",
 			ajaxType: "POST",
@@ -256,7 +275,7 @@ var Main = PFT.Util.Class({
 			},
 			trigger: $("#" + id),
 			filter: false,
-			defaultVal:coupon_id ? coupon_id : "",
+			defaultVal: coupon_id ? coupon_id : "",
 			adaptor: function (res) {
 				$(".selectOptionUl").css("padding-left", 0);
 				var result = { code: 200 };
@@ -264,19 +283,16 @@ var Main = PFT.Util.Class({
 				if (!list) {
 					return result;
 				}
-
 				var newCouList = [];
 				for (var i = 0; i < list.length; i++) {
-					
+
 					newCouList.push({
 						id: list[i].id,
 						title: list[i].coupon_name
 					});
-					
 				}
 				// console.log(newList)
 				result["data"] = newCouList;
-			
 				return result;
 			},
 		});
@@ -288,46 +304,51 @@ var Main = PFT.Util.Class({
 	//转换日期为时间戳
 	formatDate: function (date) {
 		var timestamp = Date.parse(new Date(date));
-		timestamp = timestamp / 1000;
-		return timestamp;
+		var timestamps = timestamp / 1000;
+		return timestamps;
 	},
-
+	//写入ueditor
+	setContent: function (share_type, content) {
+		if (share_type == 1) {
+			this.ue1.setContent(content);
+		} else {
+			this.ue2.setContent(content);
+		}
+	},
 	//保存新增优惠活动
 	saveAddNewActivity: function (e) {
 		var _this = this;
 		var tarBtn = $(e.currentTarget);
 		var type = tarBtn.attr("data-type");
-		var spid=tarBtn.attr("data-spid")
+		var spid = tarBtn.attr("data-spid");
+		var mkid = tarBtn.attr("data-mkid");
 		var params = {};
 		var param;
 		var title = $("#title").val();
 		var beginDate = _this.formatDate($("#beginDate").val());
 		var endDate = _this.formatDate($("#endDate").val());
-		var only_member=$(".inp-limit:checked").val();
-		console.log(only_member)
-		if (!title) return PFT.Util.STip("fail", "活动名称不为空!", 3000);
-		if (!beginDate) return PFT.Util.STip("fail", "活动起始时间不为空!", 3000);
-		if (!endDate) return PFT.Util.STip("fail", "活动截止时间不为空!", 3000);
-		if (beginDate > endDate) return PFT.Util.STip("fail", "起始时间不能迟于截止时间!", 3000);
-		if(!only_member) PFT.Util.STip("fail", "请选择是否仅限会员卡!", 3000);
+		var only_member = $(".inp-limit:checked").val();
+		if (!title) return STip("fail", "活动名称不为空!", 3000);
+		if (!beginDate) return STip("fail", "活动起始时间不为空!", 3000);
+		if (!endDate) return STip("fail", "活动截止时间不为空!", 3000);
+		if (beginDate > endDate) return STip("fail", "起始时间不能迟于截止时间!", 3000);
+		if (!only_member) STip("fail", "请选择是否仅限会员卡!", 3000);
 		switch (type) {
 			case '1':
 				param = _this.getTypeAParam();
-
 				break;
 			case '2':
 				param = _this.getTypeBParam();
-
 				break;
 			case '3':
 				param = _this.getTypeCParam();
-
 				break;
 		}
 		params = param;
-		params["spid"]=spid;
+		params["spid"] = spid;
+		params["id"] = mkid;
 		params["activity_name"] = title;
-		params["only_member"]=only_member;
+		params["only_member"] = only_member;
 		params["bt"] = beginDate;
 		params["et"] = endDate;
 		params["share_type"] = type;
@@ -340,17 +361,16 @@ var Main = PFT.Util.Class({
 		var couponId = $("#couponId").attr("data-id");
 		var couponNum = $("#couponNum").val();
 		var uecontent = this.ue1.getContent();
-		var imgPath = $("#image_path").attr("data-path");
+		var imgPath = $("#imagePath").attr("src");
 		var reg = /^([1-9]\d{0,5}|999999)$/;
 		var param;
-		if (!couponId) return PFT.Util.STip("fail", "优惠券ID不为空!", 3000);
-		if (!reg.test(couponNum)) return PFT.Util.STip("fail", "赠券张数在1~999999之间!", 3000);
+		if (!couponId) return STip("fail", "优惠券ID不为空!", 3000);
+		if (!reg.test(couponNum)) return STip("fail", "赠券张数在1~999999之间!", 3000);
 		param = {
-			coupon_id:couponId,
-			relation_pid:"0" ,
+			coupon_id: couponId,
+			relation_pid: "0",
 			coupon_num: couponNum,
-			spid:"",
-			red_pack_money:"",
+			red_pack_money: "",
 			content: uecontent,
 			thumb: imgPath || ""
 		}
@@ -363,12 +383,11 @@ var Main = PFT.Util.Class({
 		var imgPath = $("#imagePath_2").attr("data-path");
 		var param;
 		var reg = /^([0-9]\d{0,9}|1000000000)$/;
-		if (!reg.test(red_pack_money)) return PFT.Util.STip("fail", "红包金额在0~10亿之间!", 3000);
+		if (!reg.test(red_pack_money)) return STip("fail", "红包金额在0~10亿之间!", 3000);
 		param = {
-			coupon_id:"",
+			coupon_id: "",
 			relation_pid: "",
 			coupon_num: "0",
-			spid:"",
 			red_pack_money: red_pack_money,
 			content: uecontent,
 			thumb: imgPath || ""
@@ -382,37 +401,37 @@ var Main = PFT.Util.Class({
 		var couponNum = $("#couponNumber").val();
 		var proInp = $("#schProInp").attr("data-id");
 		var param;
-		var reg = /^([1-9]\d{0,5}|999999)$/;
+		var reg = /^([1-9]\d{0,5}|999999|-1)$/;
 		
-		if (!reg.test(couponNum)) return PFT.Util.STip("fail", "赠券张数在1~999999之间!", 3000);
 		coupon_num = ($("input[name='couponNum']:checked").val() == -1) ? -1 : couponNum;
-		if (!couponIdInp) return PFT.Util.STip("fail", "优惠券ID不为空!", 3000);
-		if (!proInp) return PFT.Util.STip("fail", "活动产品不为空!", 3000);
+		if (!reg.test(coupon_num)) return STip("fail", "赠券张数在1~999999之间!", 3000);
+		if (!couponIdInp) return STip("fail", "优惠券ID不为空!", 3000);
+		if (!proInp) return STip("fail", "活动产品不为空!", 3000);
 
 		param = {
-			coupon_id:couponIdInp,
+			coupon_id: couponIdInp,
 			relation_pid: proInp,
 			coupon_num: coupon_num,
-			id: "",
-			red_pack_money:"",
-			content:"",
-			thumb:"",
+			red_pack_money: "",
+			content: "",
+			thumb: "",
 		}
 		return param;
 	},
 	//保存数据请求
 	saveReq: function (param) {
-		console.log(param)
+		//console.log(param)
 		PFT.Util.Ajax('/r/product_Coupon/CreateShare', {
 			type: "POST",
 			dataType: "json",
 			params: param,
 			success: function (res) {
 				if (res.code == 200) {
-					PFT.Util.STip("success", "保存成功!", 3000);
-					window.location.href = "marketing_share_list.html";
+					STip("success", "保存成功!", 2000, function () {
+						window.location.href = "marketing_share_list.html";
+					});
 				} else {
-					PFT.Util.STip("fail", res.msg, 3000);
+					STip("fail", res.msg, 3000);
 				}
 			}
 		})
