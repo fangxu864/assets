923a711a28e9d289dc679102a2b70056ed5efa25
houxingyi <1026043046@qq.com>
2017-03-08 13:57:04 +0800

    no message

---
+++ src/car-tourists-list/index.js
@@ -0,0 +1,23 @@
+require("./index.scss");
+
+// var Template = {
+	// appBox : PFT.Util.ParseTemplate(require("./tpl/app-box.tpl"))
+// };
+var Main = PFT.Util.Class({
+
+
+	init : function(){
+        
+        console.log("123456");
+        
+
+	}
+
+});
+
+$(function(){
+
+    new Main();
+
+});
+
