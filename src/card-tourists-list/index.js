f93fde489a6f6d08254e199632d0b82aed4d387b
houxingyi <1026043046@qq.com>
2017-03-16 14:08:57 +0800

    no message

--- src/card-tourists-list/index.js
+++ src/card-tourists-list/index.js
@@ -3,7 +3,7 @@ require("./index.scss");
 //组件
 var simulate = require("./simulate");
 var Pagination = require("COMMON/modules/pagination-x");
-var Calendar = require("COMMON/modules/calendar");   //没有精确到秒
+// var Calendar = require("COMMON/modules/calendar");   //没有精确到秒
 var Select = require("./select_scroll");
 var Loading = require("COMMON/js/util.loading.pc.js"); 
 
@@ -16,26 +16,26 @@ var listTpl = require("./tpl/list.xtpl");
 //service层
 var GetList = require("./service/GetList_service.js");
 
-
-
 var Main = PFT.Util.Class({
 
 	container : $("#cardTouristsListWrap"),
 	EVENTS : {
 		"click #operator" : "onOperatorClick",  //操作员
-		"click #checkOut" : "onCheckOut"  //查询
+		"click #checkOut" : "onCheckOut",  //查询
+		"click .tdMoreBtn" : "onMoreBtn", //更多
+		"click #export" : "onExport"
  	},
 	init : function(){
 
 		var that = this;
+		this.page = 1;//页数默认为第一页
+		this.exports = "0" ;//默认不导出
 		//在发送请求之前拦截请求,模拟数据
 		simulate.init();
 		//处理操作员
 		this.handleOperator(); 
 		//处理状态
 		this.handleStatus();
-		//处理列表
-		// this.handleList();
 		//默认激活日期
 		var NowDate = new Date();
 		var year = NowDate.getFullYear();
@@ -49,24 +49,24 @@ var Main = PFT.Util.Class({
 		//日历组件部分
 		var datapicker = new Datapicker();
 		$(".actTimein").on("click",function(){
-			datapicker.show(nowDate,{
+			datapicker.open( nowDate ,{
 				picker : $(".actTimein")
 			});	
 		})
 		$(".actTimeout").on("click",function(){
-			datapicker.show(nowDate,{
+			datapicker.open( nowDate ,{
 				picker : $(".actTimeout")
 			});	
 		})
 		//分页器组件部分
 		var Pagination = require("COMMON/modules/pagination-x");
-		var pagination = new Pagination({
+		this.pagination = new Pagination({
 			container : "#pagCon",  //必须，组件容器id
 			count : 7,                //可选  连续显示分页数 建议奇数7或9
 			showTotal : true,         //可选  是否显示总页数
 			jump : true	              //可选  是否显示跳到第几页
 		});
-		pagination.on("page.switch",function(toPage,currentPage,totalPage){
+		this.pagination.on("page.switch",function(toPage,currentPage,totalPage){
 
 			// toPage :      要switch到第几页
 		    // currentPage : 当前所处第几页
@@ -75,15 +75,32 @@ var Main = PFT.Util.Class({
 			// console.log(toPage);
 			// console.log(currentPage);
 			// console.log(totalPage);
+
+			that.page = toPage ;
 			that.handleList();
 
-			pagination.render({current:toPage,total:10});
+			// that.pagination.render({current:toPage,total:totalPage});
 
 		})
-		// 主方法：(也是唯一对外显露的方法) 把分页器当前状态渲染到页面上
-		pagination.render({current:1,total:10});
 
 	},
+	//导出
+	onExport : function(){
+
+		this.exports = "1" ;//默认不导出
+
+		this.onCheckOut();
+
+	},
+	onMoreBtn : function(e){
+		var target = $(e.target);
+		var nowTr = target.parent().parent();
+		var tdItems = nowTr.find(".tdItem");
+		var eItem = nowTr.find(".tdEmpty");
+		target.css("display","none");
+		eItem.css("display","none");
+		tdItems.css("display","block");
+	}, 
 	//获得当天的时间
 	getNowFormatDate : function(){
 		var date = new Date();
@@ -97,49 +114,54 @@ var Main = PFT.Util.Class({
 		if (strDate >= 0 && strDate <= 9) {
 			strDate = "0" + strDate;
 		}
+		var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours() ;
+		var min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes() ;
+		var second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds() ;
 		var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
-				+ " " + date.getHours() + seperator2 + date.getMinutes()
-				+ seperator2 + date.getSeconds();
+				+ " " + hours + seperator2 + min
+				+ seperator2 + second ;
 		return currentdate;
 	},
 	handleList : function(){
-		
 		var html = Loading("请稍等");
 		var that = this;
 
 		//模拟数据
-		GetList({},{
-			loading : function(){
-				$("table.accountList").html(html);
-			},
-			complete : function(){
-			},
-			success : function(res){
-				console.log(res);
-				var code = res.code;
-				var list = res.data.list;
-				var msg = res.msg;
+		// GetList({},{
+		// 	loading : function(){
+		// 		$("#tempBox").css("display","none");
+		// 		$("table.accountList").html(html);
+		// 	},
+		// 	complete : function(){
+		// 	},
+		// 	success : function(res){
+		// 		console.log(res);
+		// 		var code = res.code;
+		// 		var list = res.data.list;
+		// 		var msg = res.msg;
 
-				console.log(list);
+		// 		console.log(list);
 
-				that.renderList(list);  //渲染列表
+		// 		that.renderList(list);  //渲染列表
 
-			},
-			//200但无数据
-			empty : function(){
-			},
-			//不是200
-			fail : function(){
-			}
-		});
+		// 	},
+		// 	//200但无数据
+		// 	empty : function(){
+		// 	},
+		// 	//不是200
+		// 	fail : function(){
+		// 	}
+		// });
 
 
-		return false
+		// return false
+
 
 		PFT.Util.Ajax("/r/product_parkcard/getTouristList",{
 			type : "POST",
 			dataType : "json",
 			params : {
+
 				opid : that.OID, 
 				begin : that.beginStamp,
 				end : that.endStamp,
@@ -147,19 +169,39 @@ var Main = PFT.Util.Class({
 				export : that.exports,
 				physicsno : that.CardId,
 				page : that.page,
-				pagesize : 15 
+				pagesize : 2
+
+				//调试分页用
+				// begin : 1389542769,
+				// end : 1489542879,
+				// status : 0,
+				// page : that.page,  //当前页数
+				// pagesize : 2  //每页数据条数
+
 			},
 			loading : function(){
+				$("#tempBox").css("display","none");
+				$("table.accountList").html(html);
 			},
 			complete : function(){
 			},
 			success : function(res){
 				console.log(res);
+				var code = res.code;
+				var data = res.data
+				var list = data.list;
+				var msg = res.msg;
+				that.renderList(list);  //渲染列表
+				var topage = data.page;
+				var total = data.total;
+				//渲染分页器
+				that.pagination.render({current:that.page,total:total});
+				that.transAllTimeStamp();
+				
 			},
 			timeout : function(){ alert("请求起时") },
 			serverError : function(){ alert("请求出错")}
 		})
-
 
 	},
 	renderList : function(list){
@@ -169,15 +211,19 @@ var Main = PFT.Util.Class({
 	},
 	//处理状态
 	handleStatus : function(){
+		var that = this;
 		$(".statusItem").on("click",function(){
 			$(this).siblings().find(".statusItemCircle").removeClass("selected");
 			$(this).find(".statusItemCircle").addClass("selected");
 			var status = $(this).attr("data-status");
 			$("#statusBox").attr("data-status",status);
+			that.onCheckOut();
 		});
 	},
 	//处理操作员
 	handleOperator : function(){
+
+		var that = this;
 
 		PFT.Util.Ajax("/r/product_parkcard/getOpId",{
 			type : "POST",
@@ -218,7 +264,8 @@ var Main = PFT.Util.Class({
 		}
 	},
 	//查询
-	onCheckOut : function(page){
+	onCheckOut : function(){
+
 		var beginTime = $("#actTimein").val(); //有默认时间
 		var endTime = $("#actTimeout").val(); //有默认时间
 		//化为时间戳
@@ -226,7 +273,7 @@ var Main = PFT.Util.Class({
 		this.endStamp = this.parseDateToStamp(endTime);
 		//物理卡号
 		// var CardId = $(".solidCardId").val();//物理卡号Id
-		this.CardId ="12312312312";   //模拟固定卡号
+		this.CardId ="12345685";   //opid3384的模拟固定卡号
 		//操作员
 		this.OID = $("#operator").attr("data-id");
 		if( this.OID == "0"){
@@ -241,27 +288,44 @@ var Main = PFT.Util.Class({
 		}else if(status == "used"){
 			this.status = "2";
 		}
-		//是否导出
-		this.exports = "0" ;//默认为0
-		//页数默认为1
-		if(!page){
-			this.page = "1" ;
-		}
 
+		if(this.exports == "1"){
+			console.log("导出");
+			this.getExport();
+		}else{
+			//请求列表
+			this.handleList();
+		}
 		
 
-		//模拟请求
+	},
 
-		this.handleList();
+	getExport : function(){
 
+		$.ajax({
 
-		//时间cuo化为日期开始//用于渲染列表
-		// beginStamp = Number(beginStamp);
-		// beginStamp = new Date(beginStamp*1000);
-		// var newDate = this.parseStampToDate(beginStamp);
-		//时间cuo化为日期结束
+			type : "POST",
+			dataType : 
 
 
+		});
+
+	},
+	transAllTimeStamp : function(){ //改变列表里的时间cuo
+		var that = this;
+		var stamps = $(".timeStamp");
+		stamps.each(function(i,item){
+			var stamp = stamps.eq(i).text();
+			var time = trans(stamp);
+			console.log(time);
+			stamps.eq(i).text(time);
+		});
+		function trans(Stamp){
+			var time = Number(Stamp);
+			time = new Date(time*1000);
+			var newDate = that.parseStampToDate(time);
+			return newDate
+		}
 	},
 
 	parseDateToStamp : function(date){
