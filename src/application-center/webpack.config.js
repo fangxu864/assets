c0e410666ac07f451711fbc206b7bf1d5e9716b6
moderations <40919157@qq.com>
2017-03-17 13:50:17 +0800

    应用中心首页推荐标签css调整

--- src/application-center/webpack.config.js
+++ src/application-center/webpack.config.js
@@ -10,13 +10,13 @@ var output = require("../../task-webpack/getOutput")(env);
 var config = require("../../task-webpack/config")({
 	entry : {
     	//应用中心 - 首页
-    	"appcenter_index" : "./src/application-center/index/index.js",
+    	"appcenter_index" : "./src/application-center/index/index.js"
         //应用中心 - 详情
-        "appcenter_details" : "./src/application-center/details/index.js",
+        // "appcenter_details" : "./src/application-center/details/index.js",
         //应用中心 - 应用费用配置列表
         // "appcenter_chargelist" : "./src/application-center/charge-list/index.js",
         //应用中心 - 应用属性配置
-        "appcenter_appsetting" : "./src/application-center/app-setting/index.js"
+        // "appcenter_appsetting" : "./src/application-center/app-setting/index.js",
         //应用中心 - 应用费用设置
         // "appcenter_chargesetting" : "./src/application-center/charge-setting/index.js",
         //应用中心 - 模块列表
