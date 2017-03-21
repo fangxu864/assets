a9a1ec7c0e7c7609eb9b9fc92f9728781a74c3d1
houxingyi <1026043046@qq.com>
2017-03-21 10:50:59 +0800

    no message

--- src/delay_refund/index.js
+++ src/delay_refund/index.js
@@ -1,32 +1,35 @@
 
 require("./index.scss")
 
-
 var listTpl = require("./list.xtpl");
 console.log("延迟退款");
 
 var Main =  PFT.Util.Class({
-    // container : $("#container"),  
+    container : $("#delayRefund"),  
     EVENTS : {                    
+		"click #searchBtn2" : "onsearch"
     },
     init : function(opt){         
-
-
 		this.listTemplate = PFT.Util.ParseTemplate(listTpl);  //预解析模板，缓存在template中   template是一个方法
-		
-
-
-		this.send();
     },
-	send : function(){
+	onsearch : function(){
+
+		// var ordernum = $("#ordernum").val();
+		// if( ordernum == ""){
+		// 	alert("请填写订单");
+		// 	return false
+		// }
+
+		var ordernum = "4005526";		
 
 		var that = this;
-
 		//可以用
 		PFT.Util.Ajax("/r/Order_Handler/refundList",{
 			type : "POST",
 			dataType : "json",
 			params : {
+				//先为空
+				ordernum : ordernum
 			},
 			loading : function(){
 			},
@@ -34,22 +37,14 @@ var Main =  PFT.Util.Class({
 			},
 			success : function(res){
 				console.log(res);
-
 				var html = that.listTemplate({ list : res.data });
-
 				$("#tbody").html(html);
-
 			},
 			timeout : function(){ alert("请求起时") },
 			serverError : function(){ alert("请求出错")}
 		})
 	}
 });
-
-
-
-
-
 
 $(function(){
 	new Main()
