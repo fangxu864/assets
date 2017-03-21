df5656286a7cdd19d63055ab0f5037ca427daaff
huangzy <huangzhiyang166@gmail.com>
2017-03-21 16:36:40 +0800

    no message

--- src-pc/webpack.config.js
+++ src-pc/webpack.config.js
@@ -36,33 +36,25 @@
  * 随着以后项目越来越多，webpack.config.js里 entry字段里的入口文件将会越来越多，
  * 此时如果你要编辑你的项目，建议先将其它项目的入口注释掉，否则编译速度将会极慢
  * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
  */
-
-
-
-
-
 var env = require("../task-webpack/getNodeENV");
 var path = require("path");
 var plugins = require("../task-webpack/getPlugins")(env);
 var output = require("../task-webpack/getOutput")(env);
-var config = require("../task-webpack/config")({
-	entry : {
-		//新版首页
-		// "home.pc" : "./src-pc-rebuild/home/index.js",
 
-		//新版首页 右边栏点击更多进去的二级列表页
-		"notic.list.pc" : "./src-pc/notice-list/index.js"
-	},
+var entry = {
+	//新版首页
+	// "home.pc" : "./src-pc-rebuild/home/index.js",
+
+	//新版首页 右边栏点击更多进去的二级列表页
+	"notic.list.pc" : "./src-pc/notice-list/index.js"
+};
+
+
+var config = require("../task-webpack/config")({
+	entry :entry,
 	output : output,
 	plugins : plugins
 });
+
 module.exports = config;
