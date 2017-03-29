ae8761c5b4109101e81e710750af6c903faf7ab9
WangTong <895522268@qq.com>
2017-03-29 10:16:18 +0800

    修改order-fill地址

--- src_mobile/B/order-fill/index.js
+++ src_mobile/B/order-fill/index.js
@@ -302,7 +302,7 @@ var Order_fill = PFT.Util.Class({
 				var ordernum = res.ordernum;
 				// var ordernum = "4003823";//写死的
 
-				var url = "http://wx.12301.local/html/order_pay_b.html?h="+host+"&"+"ordernum="+ordernum;
+				var url = "http://wx.12301.cc/html/order_pay_b.html?h="+host+"&"+"ordernum="+ordernum;
 
 				//跳转支付页面
 				// setTimeout(function(){
