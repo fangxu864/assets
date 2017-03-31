a7ad43c0ab42d960f4aba4bf629b871b50fbe9df
fangxu <864109504@qq.com>
2017-03-31 16:20:23 +0800

    200

--- src/sale_reportform/report_cancel/index.js
+++ src/sale_reportform/report_cancel/index.js
@@ -327,7 +327,7 @@ var Book_form={
 
 
         this.bind();
-        _this.queryState_box.show().html('<div style="text-align: center;line-height: 300px;font-size: 16px;color: #c3c3c3;">请选择查询条件再点击查询按钮进行查询</div>');
+        _this.queryState_box.show().html('<div style="text-align: center;line-height: 200px;font-size: 16px;color: #c3c3c3;">请选择查询条件再点击查询按钮进行查询</div>');
     },
     bind:function () {
         var _this=this;
