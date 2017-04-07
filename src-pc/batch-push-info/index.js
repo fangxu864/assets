654bf3784c963961601c1baa726105f851f2974b
houxingyi <1026043046@qq.com>
2017-04-07 15:04:34 +0800

    单选框多选框

--- src-pc/batch-push-info/index.js
+++ src-pc/batch-push-info/index.js
@@ -8,27 +8,61 @@ var Main = PFT.Util.Class({
     init : function(){
         console.log("发布编辑");
 
+        //下拉框
         this.handleSelect();
-
-
-
         // 单选radio
-
-        this.handleRadio();
-
-
+        this.handleRadio({
+            fa : $(".infoReceiver"),
+            ori : "rows"
+        });
+        this.handleRadio({
+            fa : $(".pushWay"),
+            ori : "columns"
+        });
+        // 多选checkbox
+        this.handleCheckBox();
 
         
 
 
-
-
-        // 多选checkbox
-
-
-
     },
+    handleCheckBox : function(){
+        var that = this;
+       $(".checkIcon").on("click",function(e){
+            var target = $(e.target);
+            if( target.attr("class") == "checkIcon active" ){
+                target.removeClass("active");        
+            }else{
+                target.addClass("active");        
+            }
+       });     
 
+    },    
+
+    handleRadio : function(opt){
+        var that = this;
+        var fa = opt.fa;
+        var ori = opt.ori;
+        var icons = fa.find(".radioIcon"); 
+        if(ori == "rows"){
+            icons.each(function(i,item){
+                var nowItem = $(item).parent();
+                nowItem.css("float","left");
+            });
+        }else if( ori == "columns" ){
+            icons.each(function(i,item){
+                var nowItem = $(item).parent();
+                nowItem.css("float","none");
+            });
+        }
+        icons.on("click",function(e){
+            var target = $(e.target);
+            target.addClass("active");
+            var sib = target.parent().siblings();
+            var bro = sib.find(".radioIcon");
+            bro.removeClass("active");
+        });
+    },
     handleSelect : function(){
         var input = $("#infoTypeInput");
         var inputH = input.css("height");
