f163bec0b30ed316f83afcd526686a49730705fe
huangzhiyang <348845730@qq.com>
2017-04-10 17:08:07 +0800

    no message

---
+++ src-pc/order-booking/module/footTotal/index.js
@@ -0,0 +1,16 @@
+require("./index.scss");
+var Tpl = require("./index.xtpl");
+var FootTtotal = PFT.Util.Class({
+    init : function(opt){
+
+    },
+    render : function(data){
+        
+    },
+    disableSubmit : function(){
+
+    },
+    enableSubmit : function(){
+        
+    }
+})
