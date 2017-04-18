1602ff1c3f5981dba8aff4cad92c31f76df862b4
moderations <40919157@qq.com>
2017-04-18 17:54:59 +0800

    用户中心

---
+++ buildTask/build.local.mb.js
@@ -0,0 +1,18 @@
+
+
+var buildCore = require("./core/build.core");
+
+
+buildCore({
+    env : "local",
+    isMobile : true
+})
+
+
+
+
+// var buildBase = require("./base/build.base.js");
+// buildBase({
+//     env : "local",
+//     srcDir : "src-pc"
+// })
