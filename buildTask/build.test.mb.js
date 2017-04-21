22cf4572e873b10a6d5dca8d24392e8595a90a86
huangzhiyang <348845730@qq.com>
2017-04-21 14:12:12 +0800

    no message

---
+++ buildTask/build.test.mb.js
@@ -0,0 +1,18 @@
+
+
+var buildCore = require("./core/build.core");
+
+
+buildCore({
+    env : "test",
+    isMobile : true
+})
+
+
+
+
+// var buildBase = require("./base/build.base.js");
+// buildBase({
+//     env : "local",
+//     srcDir : "src-pc"
+// })
