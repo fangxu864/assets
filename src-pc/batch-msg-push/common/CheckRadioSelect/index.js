b4db64ff04bed1744e027b1fdae1e8355e8e7888
huangzhiyang <348845730@qq.com>
2017-05-23 14:08:09 +0800

    no message

--- src-pc/batch-msg-push/common/CheckRadioSelect/index.js
+++ src-pc/batch-msg-push/common/CheckRadioSelect/index.js
@@ -1,6 +1,23 @@
+var Util = PFT.Util;
+var isObject = Util.isObject;
+var isArray = Util.isArray;
+var isUndefined = Util.isUndefined;
 var CheckRadioSelect = PFT.Util.Class({
     init : function(opt){
+        this.opt = opt;
+        console.log(this.container);
+        console.log(this.opt);
+    },
+    _createOption : function(){
+        var option = this.opt.option;
+        var type = option.type;
+        var name = option.name;
+        var html = "";
+        if(isArray(option) && option.length){
 
+        }else if(isObject(option)){
+
+        }
     },
     disable : function(){
 
