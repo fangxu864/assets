09ca6b5fbca29341a47d4a6da1bd99e6a6355415
huangzhiyang <348845730@qq.com>
2017-05-23 18:18:22 +0800

    no message

--- src-pc/batch-msg-push/common/CheckRadioSelect/index.js
+++ src-pc/batch-msg-push/common/CheckRadioSelect/index.js
@@ -1,30 +1,198 @@
+/*
+ * @Author: huangzhiyang 
+ * @Date: 2017-05-23 18:15:09 
+ * @Last Modified by:   huangzhiyang 
+ * @Last Modified time: 2017-05-23 18:15:09 
+ */
+require("./index.scss");
 var Util = PFT.Util;
 var isObject = Util.isObject;
 var isArray = Util.isArray;
 var isUndefined = Util.isUndefined;
+var isString = Util.isString;
+var isNumber = Util.isNumber;
+var isFunction = Util.isFunction;
 var CheckRadioSelect = PFT.Util.Class({
+    EVENTS : {
+        "click .checkRadioOpt" : "onCheckRadioOptClick"
+    },
+    state : {
+        hasInit : false
+    },
     init : function(opt){
         this.opt = opt;
-        console.log(this.container);
-        console.log(this.opt);
+        this._bindEvent();
+    },
+    onCheckRadioOptClick : function(e){
+        var tarItem = $(e.currentTarget);
+        var key = tarItem.attr("data-key");
+        var type = this.opt.type;
+        if(!tarItem.hasClass("disable")){
+            if(type!=="checkbox"){
+                tarItem.addClass("checked").siblings().removeClass("checked");
+            }else{
+                tarItem.toggleClass("checked");
+            }
+        }
+        var params = {
+            key : key,
+            text : this.getText(key),
+            disable : tarItem.hasClass("disable"),
+            tarOption : tarItem,
+            checked : this.getChecked(),
+            unChecked : this.getUnChecked()
+        };
+        this.opt.onSelect.call(this,params);
+        this.trigger("select",params);
+    },
+    _bindEvent : function(){
+        var events = this.opt.EVENTS;
+        var container = this.container;
+        for(var i in events){
+            (function(i){
+
+                // events = {
+                //     "click .btn a" : "onClick",
+                //     //如果有多个事件名，以下2种写法都支持，但推荐使用第1种，这也是jquery的写法
+                //     "input propertychange #input" : "onInputChange",
+                //     "input,propertychange #input" : "onInputChange"
+                // }
+
+                var matched = i.match(/(([a-z]+(\s|,)*)+)(\s)(.*)/);
+                var eventType = matched[1];
+                var selector = matched[5];
+                eventType = eventType.replace(","," ");
+                var handler = events[i];
+                var handlerType = typeof handler;
+                if(handlerType!=="function" && handlerType!=="string") return;
+                if(handlerType=="string") handler = Option[handler];
+                if(typeof handler!=="function") return;
+                if(selector){
+                    container.on(eventType,selector,function(e){
+                        handler.call(that,e);
+                    });
+                }else{
+                    pocontainerper.on(eventType,function(e){
+                        handler.call(that,e);
+                    });
+                }	
+            })(i);
+        }
     },
     _createOption : function(){
+        var that = this;
         var option = this.opt.option;
         var type = option.type;
         var name = option.name;
         var html = "";
-        if(isArray(option) && option.length){
+        if(!isArray(option) || option.length==0) return false;
+        for(var i=0,len=option.length; i<len; i++){
+            var opt = option[i];
+            var key = opt.key;
+            var text = isFunction(opt.text) ? opt.text() : opt.text;
+            var id = that._createID(key);
+            html += '<li data-key="'+key+'" id="checkRadioOpt_'+id+'" class="checkRadioOpt checkRadioOpt_'+id+'">';
+			html += '	<span class="inp"></span>';
+			html += '	<span class="label">'+text+'</span>';
+			html += '</li>';
+        }
+        return html;
+    },
+    _createID : function(key){
+        var opt = this.opt;
+        var type = opt.type;
+        var name = opt.name;
+        var id = type + "_" + name + "_" + key;
+        return id;
+    },
 
-        }else if(isObject(option)){
-
+    //===================================================================
+    //=======暴露以下方法给外部使用
+    //===================================================================
+    render : function(){
+        var opt = this.opt;
+        var layout = {
+            h : "land",
+            v : "ver"
+        }[opt.layout];
+        var type = opt.type;
+        var cls = layout + " " + type;
+        var html = '';
+        var optHtml = this._createOption();
+        if(!optHtml) return false;
+        html += '<ul class="checkRadioWrap '+cls+'">';
+        html += optHtml;
+        html += '</ul>';
+        this.container.html(html);
+        if(!this.state.hasInit){
+            this.state.hasInit = true;
+            var checked = this.opt.checked;
+            if((isString(checked) && checked!="") || isNumber(checked)){
+                var id = this._createID(checked);
+                var tarOption = $("#checkRadioOpt_"+id);
+                if(tarOption.length) tarOption.trigger("click");
+            }
         }
     },
-    disable : function(){
-
+    disable : function(key){
+        if(isUndefined(key)){
+            this.container.find(".checkRadioOpt").removeClass("checked").addClass("disable");
+        }else{
+            var tarItem = this.container.find(".checkRadioOpt_"+this._createID(key));
+            if(tarItem.length) tarItem.removeClass("checked").addClass("disable");
+        }
     },
-    enable : function(){},
-    getValue : function(){},
-    setValue : function(){}
+    enable : function(key){
+        if(isUndefined(key)){
+            this.container.find(".checkRadioOpt").removeClass("disable");
+        }else{
+            var tarItem = this.container.find(".checkRadioOpt_"+this._createID(key));
+            if(tarItem.length) tarItem.removeClass("disable");
+        }
+    },
+    getText : function(key){
+        if(isUndefined(key)) return false;
+        var option = this.opt.option;
+        var result = "";
+        for(var i=0; i<option.length; i++){
+            var opt = option[i];
+            var id = opt.key;
+            var text = isFunction(opt.text) ? opt.text() : opt.text;
+            if(id==key){
+                result = text;
+                break;
+            }
+        }
+        return result;
+    },
+    getValue : function(){
+        var result = [];
+        this.container.find(".checkRadioOpt").filter(function(){
+            return $(this).hasClass("checked");
+        }).each(function(){
+            result.push($(this).attr("data-key"));
+        });
+        return result.join(",");
+    },
+    getChecked : function(){
+        var result = [];
+        this.container.find(".checkRadioOpt").each(function(){
+            var item = $(this);
+            if(item.hasClass("checked")) result.push(item.attr("data-key"));
+        })
+        return result.join(",");
+    },
+    getUnChecked : function(){
+        var result = [];
+        this.container.find(".checkRadioOpt").each(function(){
+            var item = $(this);
+            if(!item.hasClass("checked")) result.push(item.attr("data-key"));
+        })
+        return result.join(",");
+    },
+    destroy : function(){
+        this.container.off().remove();
+    }
 });
 
 
@@ -33,8 +201,10 @@ module.exports = function(container,opt){
     opt = opt || {};
     var def = {
         type : "checkbox",
-        name : "",
+        layout : "h",  //h-横向   v-竖向
         checked : "",
+        name : "",
+        EVENTS : {},
         onSelect : function(){},
         option : []
     };
@@ -45,8 +215,5 @@ module.exports = function(container,opt){
 
     opt["container"] = container;
 
-
-
-
-    return new CheckRadioSelect(opt)
+    return new CheckRadioSelect(opt);
 }
--- src-pc/batch-msg-push/edit-create/index.js
+++ src-pc/batch-msg-push/edit-create/index.js
@@ -4,6 +4,31 @@ var Common = require("../common/index.js");
 
 var CheckRadioSelect = Common.CheckRadioSelect;
 
-var c = CheckRadioSelect($("body"),{
-    
+var c = CheckRadioSelect("#receiverCheckRadioSelectWrap",{
+    type : "checkbox",
+    name : "recType",
+    checked : "1",
+    option : [{
+        key : 1,
+        text : "系统自动筛选"
+    },{
+        key : 2,
+        text : "指定会员"
+    }],
+    onSelect : function(data){
+        
+    }
+});
+c.on("select",function(data){
+    console.log(data);
 })
+
+
+var Main = {
+    init : function(){
+        c.render();
+    }
+};
+
+
+$(function(){ Main.init()})
